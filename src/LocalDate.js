9ae31fc58faf2ca852d2d90faae1562e418e1407
Philipp Thürwächter <pithu@thuerwaechter.de>
2015-10-17 12:49:16 +0200

    remove html tags from doc

--- src/LocalDate.js
+++ src/LocalDate.js
@@ -6,12 +6,12 @@
 /**
  * A date without a time-zone in the ISO-8601 calendar system,
  * such as {@code 2007-12-03}.
- * <p>
+ *
  * {@code LocalDate} is an immutable date-time object that represents a date,
  * often viewed as year-month-day. Other date fields, such as day-of-year,
  * day-of-week and week-of-year, can also be accessed.
  * For example, the value "2nd October 2007" can be stored in a {@code LocalDate}.
- * <p>
+ *
  * This class does not store or represent a time or time-zone.
  * Instead, it is a description of the date, as used for birthdays.
  * It cannot represent an instant on the time-line without additional information
@@ -40,7 +40,7 @@ export class LocalDate {
 
     /**
      * Outputs this date as a {@code String}, such as {@code 2007-12-03}.
-     * <p>
+     *
      * The output will be in the ISO-8601 format {@code uuuu-MM-dd}.
      *
      * @return a string representation of this date, not null
