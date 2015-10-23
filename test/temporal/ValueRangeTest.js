10cf318d10d1970ac448578656f5faee8384c319
Philipp Thürwächter <pithu@thuerwaechter.de>
2015-10-23 22:54:32 +0200

    switch test to a month value range

--- test/temporal/ValueRangeTest.js
+++ test/temporal/ValueRangeTest.js
@@ -3,17 +3,19 @@ import {ValueRange} from '../../src/temporal/ValueRange';
 
 describe('ValueRange', () => {
 
-    it('should define a ValueRange', () => {
-        var minMaxValueRange = ValueRange.of(0,10);
+    it('should define a min/ max ValueRange', () => {
+        var minMaxValueRange = ValueRange.of(1,12);
 
-        expect(minMaxValueRange.isValidValue(0)).to.be.true;
-        expect(minMaxValueRange.isValidValue(2)).to.be.true;
-        expect(minMaxValueRange.isValidValue(10)).to.be.true;
-        expect(minMaxValueRange.isValidValue(-1)).to.be.false;
-        expect(minMaxValueRange.isValidValue(11)).to.be.false;
+        expect(minMaxValueRange.isValidValue(0)).to.equal(false);
+        expect(minMaxValueRange.isValidValue(2)).to.equal(true);
+        expect(minMaxValueRange.isValidValue(10)).to.equal(true);
+        expect(minMaxValueRange.isValidValue(13)).to.equal(false);
 
         expect(minMaxValueRange.checkValidValue(10)).to.be.undefined;
 
     });
+
+
+    // TODO tests missing
 
 });
