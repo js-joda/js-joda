b7629a763a944b097bbb094ca14b0cc4a9f653de
Philipp Thürwächter <pithu@thuerwaechter.de>
2015-10-23 22:59:44 +0200

    add the ChronoFields day, month and year

---
+++ src/temporal/ChronoField.js
@@ -0,0 +1,41 @@
+import { ValueRange } from './ValueRange';
+
+class  ChronoField {
+
+    constructor(name, baseUnit, rangeUnit, range, displayNameKey) {
+        this.name = function () {
+            return name;
+        };
+        this.baseUnit = function () {
+            return baseUnit;
+        };
+        this.rangeUnit = function () {
+            return rangeUnit;
+        };
+        this.range = function () {
+            return range;
+        };
+        this.displayNameKey = function () {
+            return displayNameKey;
+        };
+    }
+
+    checkValidValue(value) {
+        return this.range().checkValidValue(value, this.name());
+    }
+}
+
+
+export const DAY_OF_MONTH = new ChronoField(
+    "DayOfMonth", null, null, ValueRange.of(1, 28, 31), "day"
+);
+
+export const MONTH_OF_YEAR = new ChronoField(
+    "MonthOfYear", null, null, ValueRange.of(1, 12), "month"
+);
+
+export const YEAR = new ChronoField("" +
+    "Year", null, null, ValueRange.of(-999999, 999999), "year"
+);
+
+
