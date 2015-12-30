549535e8755f096bd9143e521c14a8606564dd57
Philipp Thürwächter <pithu@thuerwaechter.de>
2015-12-30 21:22:43 +0100

    first draft of Instant

---
+++ src/Instant.js
@@ -0,0 +1,54 @@
+import {DateTimeException} from './errors'
+
+// TODO verify the arbitrary values for min/ max seconds, set to about 999_999 Years for now
+const MIN_SECONDS = -30818963289600;
+const MAX_SECONDS = 30697775193600;
+
+export class Instant {
+
+    constructor(seconds, nanoOfSecond){
+        Instant.validate(seconds, nanoOfSecond);
+        this._seconds = seconds;
+        this._nano = nanoOfSecond;
+    }
+
+    epochSecond(){
+        return this._seconds
+    }
+
+    nano(){
+        return this._nano
+    }
+
+    equals(otherInstant) {
+        if(this === otherInstant){
+            return true;
+        }
+        if(otherInstant instanceof Instant){
+            return this.epochSecond() === otherInstant.epochSecond() &&
+                this.nano() === otherInstant.nano()
+        }
+        return false;
+    }
+
+    static ofEpochSecond(epochSeconds, nanoAdjustment){
+        return Instant._create(epochSeconds, nanoAdjustment);
+    }
+
+    static _create(seconds, nanoOfSecond){
+        if(seconds === 0 && nanoOfSecond === 0){
+            return Instant.EPOCH;
+        }
+        return new Instant(seconds, nanoOfSecond);
+    }
+
+    static validate(seconds, nanoOfSecond){
+        if (seconds < MIN_SECONDS || seconds > MAX_SECONDS) {
+            throw new DateTimeException("Instant exceeds minimum or maximum instant")
+        }
+    }
+}
+
+Instant.EPOCH = new Instant(0,0);
+Instant.MIN = Instant.ofEpochSecond(MIN_SECONDS, 0);
+Instant.MAX = Instant.ofEpochSecond(MAX_SECONDS, 999999999);
