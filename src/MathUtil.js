380c9e1528df89fba446670146bb30651644468c
Philipp Thürwächter <pithu@thuerwaechter.de>
2015-12-30 23:32:04 +0100

    add floorDic and floorMod untested

--- src/MathUtil.js
+++ src/MathUtil.js
@@ -5,5 +5,21 @@ export class MathUtil {
     static div(a, b) {
         return ~~(a / b);
     }
+
+    // TODO test it
+    static floorDiv(x, y){
+        var r = MathUtil.div(x, y);
+        // if the signs are different and modulo not zero, round down
+        if((x ^ y) < 0 && (r*y !== x)){
+            r--;
+        }
+        return r;
+    }
+
+    // TODO test it
+    static floorMod(x, y){
+        var r = x - MathUtil.floorDiv(x, y) * y;
+        return r;
+    }
 }
 
