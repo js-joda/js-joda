ab4fbd59c8bceda1ae9d23e7528e9c4e9408e0b9
Pattrick Hueper <phueper@hueper.net>
2015-12-31 22:59:37 +0100

    initial (empty) Year implementation... only needed for MAX_VALUE/MIN_VALUE currently

---
+++ src/Year.js
@@ -0,0 +1,37 @@
+/**
+ * A year in the ISO-8601 calendar system, such as {@code 2007}.
+ * <p>
+ * {@code Year} is an immutable date-time object that represents a year.
+ * Any field that can be derived from a year can be obtained.
+ * <p>
+ * <b>Note that years in the ISO chronology only align with years in the
+ * Gregorian-Julian system for modern years. Parts of Russia did not switch to the
+ * modern Gregorian/ISO rules until 1920.
+ * As such, historical years must be treated with caution.</b>
+ * <p>
+ * This class does not store or represent a month, day, time or time-zone.
+ * For example, the value "2007" can be stored in a {@code Year}.
+ * <p>
+ * Years represented by this class follow the ISO-8601 standard and use
+ * the proleptic numbering system. Year 1 is preceded by year 0, then by year -1.
+ * <p>
+ * The ISO-8601 calendar system is the modern civil calendar system used today
+ * in most of the world. It is equivalent to the proleptic Gregorian calendar
+ * system, in which today's rules for leap years are applied for all time.
+ * For most applications written today, the ISO-8601 rules are entirely suitable.
+ * However, any application that makes use of historical dates, and requires them
+ * to be accurate will find the ISO-8601 approach unsuitable.
+ *
+ */
+export class Year {
+    
+}
+
+/**
+ * The minimum supported year
+ */
+Year.MIN_VALUE = -999999;
+/**
+ * The maximum supported year
+ */
+Year.MAX_VALUE = 999999;
