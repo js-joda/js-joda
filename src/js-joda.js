985dd0f4fc48d0d817a0494178b2ca9dc318e3be
Philipp Thürwächter <pithu@thuerwaechter.de>
2016-01-02 21:55:18 +0100

    export more classes

--- src/js-joda.js
+++ src/js-joda.js
@@ -1,5 +1,11 @@
 import { LocalDate } from './LocalDate'
+import { Instant } from './Instant'
+import { Clock } from './Clock'
+import { ZoneOffset } from './ZoneOffset'
 
 export default {
-    LocalDate: LocalDate
+    LocalDate: LocalDate,
+    Instant: Instant,
+    Clock: Clock,
+    ZoneOffset: ZoneOffset
 }
