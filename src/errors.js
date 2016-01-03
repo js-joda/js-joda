dec08b81c5240816141fb47b029d155cac86b85a
Pattrick Hueper <phueper@hueper.net>
2016-01-03 16:35:04 +0100

    add DateTimeParseException

--- src/errors.js
+++ src/errors.js
@@ -6,6 +6,12 @@ export class DateTimeException extends ExtendableError {
     }
 }
 
+export class DateTimeParseException extends ExtendableError {
+    constructor(message = 'DateTimeParseException') {
+        super(message)
+    }
+}
+
 export class UnsupportedTemporalTypeException extends ExtendableError {
     constructor(message = 'UnsupportedTemporalTypeException') {
         super(message)
