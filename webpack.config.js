de1bfb8dcf672578ae868efe2a3b14bdc088dab3
Philipp Thürwächter <pithu@thuerwaechter.de>
2016-01-04 11:13:54 +0100

    remove superfluous config
    shouldnt make any diffference, since we start at the entrypoints only

--- webpack.config.js
+++ webpack.config.js
@@ -21,7 +21,7 @@ module.exports = {
                     path.resolve(__dirname, "src"),
                     path.resolve(__dirname, "test")
                 ],
-                test: /(src)|(test)\/.+.js$/
+                test: /.js$/
         }]
     },
     plugins: minify ? [
