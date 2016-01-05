6d052b1a52519d1d850a1dfd0b2be8912c71d2fa
Pattrick Hueper <phueper@hueper.net>
2016-01-05 12:14:25 +0100

    to make DateTimeParseException more similar to the JDK implementation, add a initCause() method, even though the cause is currently not added to the message

--- src/errors.js
+++ src/errors.js
@@ -7,8 +7,13 @@ export class DateTimeException extends ExtendableError {
 }
 
 export class DateTimeParseException extends ExtendableError {
-    constructor(message = 'DateTimeParseException') {
-        super(message)
+    constructor(message = 'DateTimeParseException', text = '', index = 0) {
+        super(message + ": " + text + ", at index: " + index)
+    }
+    
+    initCause(exception) {
+        // TODO: can we add the cause to the stacktrace??
+        this._cause = exception;
     }
 }
 
