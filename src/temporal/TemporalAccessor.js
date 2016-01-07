3a069b35937c45646987a1012c980061ca6bfb57
Pattrick Hueper <phueper@hueper.net>
2016-01-07 23:20:37 +0100

    use the threetenbp implementation as base

--- src/temporal/TemporalAccessor.js
+++ src/temporal/TemporalAccessor.js
@@ -1,7 +1,7 @@
-import {TemporalQueries} from './TemporalQueries'
+import {TemporalQueries} from './TemporalQueries';
 
 export class TemporalAccessor {
-        /**
+    /**
      * Queries this date-time.
      * <p>
      * This queries this date-time using the specified query strategy object.
@@ -14,37 +14,19 @@ export class TemporalAccessor {
      * <p>
      * The most common query implementations are method references, such as
      * {@code LocalDate::from} and {@code ZoneId::from}.
-     * Additional implementations are provided as static methods on {@link TemporalQuery}.
+     * Further implementations are on {@link TemporalQueries}.
+     * Queries may also be defined by applications.
      *
      * @implSpec
-     * The default implementation must behave equivalent to this code:
+     * Implementations of this method must behave as follows:
      * <pre>
-     *  if (query == TemporalQueries.zoneId() ||
-     *        query == TemporalQueries.chronology() || query == TemporalQueries.precision()) {
-     *    return null;
-     *  }
-     *  return query.queryFrom(this);
+        if (query == TemporalQueries.zoneId()
+            || query == TemporalQueries.chronology()
+            || query == TemporalQueries.precision()) {
+                return null;
+        }
+        return query.queryFrom(this);
      * </pre>
-     * Future versions are permitted to add further queries to the if statement.
-     * <p>
-     * All classes implementing this interface and overriding this method must call
-     * {@code TemporalAccessor.super.query(query)}. JDK classes may avoid calling
-     * super if they provide behavior equivalent to the default behaviour, however
-     * non-JDK classes may not utilize this optimization and must call {@code super}.
-     * <p>
-     * If the implementation can supply a value for one of the queries listed in the
-     * if statement of the default implementation, then it must do so.
-     * For example, an application-defined {@code HourMin} class storing the hour
-     * and minute must override this method as follows:
-     * <pre>
-     *  if (query == TemporalQueries.precision()) {
-     *    return MINUTES;
-     *  }
-     *  return TemporalAccessor.super.query(query);
-     * </pre>
-     * <p>
-     * Implementations must ensure that no observable state is altered when this
-     * read-only method is invoked.
      *
      * @param {TemporalQuery} query  the query to invoke, not null
      * @return the query result, null may be returned (defined by the query)
