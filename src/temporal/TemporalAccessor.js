c8e20f75fea2f43b8945c05a9f588cce14d159fb
Pattrick Hueper <phueper@hueper.net>
2016-01-17 14:53:24 +0100

    add range function to TemporalAccessor

--- src/temporal/TemporalAccessor.js
+++ src/temporal/TemporalAccessor.js
@@ -1,4 +1,6 @@
+import {ChronoField} from './ChronoField';
 import {TemporalQueries} from './TemporalQueries';
+import {UnsupportedTemporalTypeException} from '../errors';
 
 export class TemporalAccessor {
     /**
@@ -40,6 +42,44 @@ export class TemporalAccessor {
             return null;
         }
         return query.queryFrom(this);
+    }
+    
+    /**
+     * Gets the range of valid values for the specified field.
+     * <p>
+     * All fields can be expressed as a {@code long} integer.
+     * This method returns an object that describes the valid range for that value.
+     * The value of this temporal object is used to enhance the accuracy of the returned range.
+     * If the date-time cannot return the range, because the field is unsupported or for
+     * some other reason, an exception will be thrown.
+     * <p>
+     * Note that the result only describes the minimum and maximum valid values
+     * and it is important not to read too much into them. For example, there
+     * could be values within the range that are invalid for the field.
+     *
+     * <h3>Specification for implementors</h3>
+     * Implementations must check and handle all fields defined in {@link ChronoField}.
+     * If the field is supported, then the range of the field must be returned.
+     * If unsupported, then a {@code DateTimeException} must be thrown.
+     * <p>
+     * If the field is not a {@code ChronoField}, then the result of this method
+     * is obtained by invoking {@code TemporalField.rangeRefinedBy(TemporalAccessorl)}
+     * passing {@code this} as the argument.
+     * <p>
+     * Implementations must not alter either this object.
+     *
+     * @param {TemporalField} field  the field to query the range for, not null
+     * @return {ValueRange} the range of valid values for the field, not null
+     * @throws DateTimeException if the range for the field cannot be obtained
+     */
+    range(field) {
+        if (field instanceof ChronoField) {
+            if (this.isSupported(field)) {
+                return field.range();
+            }
+            throw new UnsupportedTemporalTypeException('Unsupported field: ' + field);
+        }
+        return field.rangeRefinedBy(this);
     }
 
 }
