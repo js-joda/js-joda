bfb714174bebc40b18e680f1a89240cdf6725e7c
Pattrick Hueper <phueper@hueper.net>
2016-01-17 14:53:02 +0100

    update ChronoField as needed by Instant and Duration, add TemporalField as basic "interface"

--- src/temporal/ChronoField.js
+++ src/temporal/ChronoField.js
@@ -1,9 +1,12 @@
+import {MAX_SAFE_INTEGER, MIN_SAFE_INTEGER} from '../MathUtil';
+import { TemporalField } from './TemporalField';
 import { ValueRange } from './ValueRange';
-import {Year} from '../Year'
+import {Year} from '../Year';
 
-export class  ChronoField {
+export class ChronoField extends TemporalField {
 
     constructor(name, baseUnit, rangeUnit, range, displayNameKey) {
+        super();
         this.name = function () {
             return name;
         };
@@ -24,12 +27,58 @@ export class  ChronoField {
     checkValidValue(value) {
         return this.range().checkValidValue(value, this.name());
     }
+    
+    /**
+     * Get the range of valid values for this field using the temporal object to
+     * refine the result.
+     * <p>
+     * This uses the temporal object to find the range of valid values for the field.
+     * This is similar to {@link #range()}, however this method refines the result
+     * using the temporal. For example, if the field is {@code DAY_OF_MONTH} the
+     * {@code range} method is not accurate as there are four possible month lengths,
+     * 28, 29, 30 and 31 days. Using this method with a date allows the range to be
+     * accurate, returning just one of those four options.
+     * <p>
+     * There are two equivalent ways of using this method.
+     * The first is to invoke this method directly.
+     * The second is to use {@link TemporalAccessor#range(TemporalField)}:
+     * <pre>
+     *   // these two lines are equivalent, but the second approach is recommended
+     *   temporal = thisField.rangeRefinedBy(temporal);
+     *   temporal = temporal.range(thisField);
+     * </pre>
+     * It is recommended to use the second approach, {@code range(TemporalField)},
+     * as it is a lot clearer to read in code.
+     * <p>
+     * Implementations should perform any queries or calculations using the fields
+     * available in {@link ChronoField}.
+     * If the field is not supported a {@code DateTimeException} must be thrown.
+     *
+     * @param {TemporalAccessor} temporal  the temporal object used to refine the result, not null
+     * @return {Va;lueRange} the range of valid values for this field, not null
+     * @throws DateTimeException if the range for the field cannot be obtained
+     */
+    rangeRefinedBy(temporal) {
+        return temporal.range(this);
+    }
+
 }
 
-ChronoField.DAY_OF_MONTH = new ChronoField("DayOfMonth", null, null, ValueRange.of(1, 28, 31), "day");
+// TODO: why can't we use ChronoUnit.NANOS, ... in these initializers??
+//ChronoField.NANO_OF_SECOND = new ChronoField('NanoOfSecond', ChronoUnit.NANOS, ChronoUnit.SECONDS, ValueRange.of(0, 999999999));
+ChronoField.NANO_OF_SECOND = new ChronoField('NanoOfSecond', null, null, ValueRange.of(0, 999999999));
 
-ChronoField.MONTH_OF_YEAR = new ChronoField("MonthOfYear", null, null, ValueRange.of(1, 12), "month");
+ChronoField.MICRO_OF_SECOND = new ChronoField('MicroOfSecond', null, null, ValueRange.of(0, 999999));
 
-ChronoField.YEAR = new ChronoField("Year", null, null, ValueRange.of(Year.MIN_VALUE, Year.MAX_VALUE), "year");
+ChronoField.MILLI_OF_SECOND = new ChronoField('MilliOfSecond', null, null, ValueRange.of(0, 999));
+
+ChronoField.DAY_OF_MONTH = new ChronoField('DayOfMonth', null, null, ValueRange.of(1, 28, 31), 'day');
+
+ChronoField.MONTH_OF_YEAR = new ChronoField('MonthOfYear', null, null, ValueRange.of(1, 12), 'month');
+
+ChronoField.YEAR = new ChronoField('Year', null, null, ValueRange.of(Year.MIN_VALUE, Year.MAX_VALUE), 'year');
+
+ChronoField.INSTANT_SECONDS = new ChronoField('InstantSeconds', null, null, ValueRange.of(MIN_SAFE_INTEGER, MAX_SAFE_INTEGER));
+    
 
 
