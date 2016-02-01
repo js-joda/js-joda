936776b6ff53bd0d259f7fdf78d8e9c0bb35ad17
Philipp Thürwächter <pithu@thuerwaechter.de>
2016-02-01 12:02:11 +0100

    renamed LICENSE.md to LICENSE

--- src/temporal/ChronoField.js
+++ src/temporal/ChronoField.js
@@ -1,7 +1,7 @@
 /**
  * @copyright (c) 2016, Philipp Thuerwaechter & Pattrick Hueper
  * @copyright (c) 2007-present, Stephen Colebourne & Michael Nascimento Santos
- * @license BSD-3-Clause (see LICENSE.md in the root directory of this source tree)
+ * @license BSD-3-Clause (see LICENSE in the root directory of this source tree)
  */
 import {MAX_SAFE_INTEGER, MIN_SAFE_INTEGER} from '../MathUtil';
 import { TemporalField } from './TemporalField';
--- src/temporal/TemporalField.js
+++ src/temporal/TemporalField.js
@@ -1,7 +1,7 @@
 /**
  * @copyright (c) 2016, Philipp Thuerwaechter & Pattrick Hueper
  * @copyright (c) 2007-present, Stephen Colebourne & Michael Nascimento Santos
- * @license BSD-3-Clause (see LICENSE.md in the root directory of this source tree)
+ * @license BSD-3-Clause (see LICENSE in the root directory of this source tree)
  */
 export class TemporalField {
     constructor() {
