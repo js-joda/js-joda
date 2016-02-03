e56532208b55458ba8b8b268378664404b6338ec
Philipp Thürwächter <pithu@thuerwaechter.de>
2016-02-03 20:34:18 +0100

    add parseSignsLenient tests

--- test/reference/format/NumberParserTest.js
+++ test/reference/format/NumberParserTest.js
@@ -253,6 +253,115 @@ describe('org.threeten.bp.TestNumberParser', () => {
         }
     });
 
+    describe('parseSignsLenient', () => {
+        var provider_parseSignsLenient = [
+            // never
+            ['0', 1, 2, SignStyle.NEVER, 1, 0],
+            ['5', 1, 2, SignStyle.NEVER, 1, 5],
+            ['50', 1, 2, SignStyle.NEVER, 2, 50],
+            ['500', 1, 2, SignStyle.NEVER, 3, 500],
+            ['-0', 1, 2, SignStyle.NEVER, 2, 0],
+            ['-5', 1, 2, SignStyle.NEVER, 2, -5],
+            ['-50', 1, 2, SignStyle.NEVER, 3, -50],
+            ['-500', 1, 2, SignStyle.NEVER, 4, -500],
+            ['-AAA', 1, 2, SignStyle.NEVER, ~1, null],
+            ['+0', 1, 2, SignStyle.NEVER, 2, 0],
+            ['+5', 1, 2, SignStyle.NEVER, 2, 5],
+            ['+50', 1, 2, SignStyle.NEVER, 3, 50],
+            ['+500', 1, 2, SignStyle.NEVER, 4, 500],
+            ['+AAA', 1, 2, SignStyle.NEVER, ~1, null],
+            ['50', 2, 2, SignStyle.NEVER, 2, 50],
+            ['-50', 2, 2, SignStyle.NEVER, ~0, null],
+            ['+50', 2, 2, SignStyle.NEVER, ~0, null],
+
+            // not negative
+            ['0', 1, 2, SignStyle.NOT_NEGATIVE, 1, 0],
+            ['5', 1, 2, SignStyle.NOT_NEGATIVE, 1, 5],
+            ['50', 1, 2, SignStyle.NOT_NEGATIVE, 2, 50],
+            ['500', 1, 2, SignStyle.NOT_NEGATIVE, 3, 500],
+            ['-0', 1, 2, SignStyle.NOT_NEGATIVE, 2, 0],
+            ['-5', 1, 2, SignStyle.NOT_NEGATIVE, 2, -5],
+            ['-50', 1, 2, SignStyle.NOT_NEGATIVE, 3, -50],
+            ['-500', 1, 2, SignStyle.NOT_NEGATIVE, 4, -500],
+            ['-AAA', 1, 2, SignStyle.NOT_NEGATIVE, ~1, null],
+            ['+0', 1, 2, SignStyle.NOT_NEGATIVE, 2, 0],
+            ['+5', 1, 2, SignStyle.NOT_NEGATIVE, 2, 5],
+            ['+50', 1, 2, SignStyle.NOT_NEGATIVE, 3, 50],
+            ['+500', 1, 2, SignStyle.NOT_NEGATIVE, 4, 500],
+            ['+AAA', 1, 2, SignStyle.NOT_NEGATIVE, ~1, null],
+            ['50', 2, 2, SignStyle.NOT_NEGATIVE, 2, 50],
+            ['-50', 2, 2, SignStyle.NOT_NEGATIVE, ~0, null],
+            ['+50', 2, 2, SignStyle.NOT_NEGATIVE, ~0, null],
+
+            // normal
+            ['0', 1, 2, SignStyle.NORMAL, 1, 0],
+            ['5', 1, 2, SignStyle.NORMAL, 1, 5],
+            ['50', 1, 2, SignStyle.NORMAL, 2, 50],
+            ['500', 1, 2, SignStyle.NORMAL, 3, 500],
+            ['-0', 1, 2, SignStyle.NORMAL, 2, 0],
+            ['-5', 1, 2, SignStyle.NORMAL, 2, -5],
+            ['-50', 1, 2, SignStyle.NORMAL, 3, -50],
+            ['-500', 1, 2, SignStyle.NORMAL, 4, -500],
+            ['-AAA', 1, 2, SignStyle.NORMAL, ~1, null],
+            ['+0', 1, 2, SignStyle.NORMAL, 2, 0],
+            ['+5', 1, 2, SignStyle.NORMAL, 2, 5],
+            ['+50', 1, 2, SignStyle.NORMAL, 3, 50],
+            ['+500', 1, 2, SignStyle.NORMAL, 4, 500],
+            ['+AAA', 1, 2, SignStyle.NORMAL, ~1, null],
+            ['50', 2, 2, SignStyle.NORMAL, 2, 50],
+            ['-50', 2, 2, SignStyle.NORMAL, 3, -50],
+            ['+50', 2, 2, SignStyle.NORMAL, 3, 50],
+
+            // always
+            ['0', 1, 2, SignStyle.ALWAYS, 1, 0],
+            ['5', 1, 2, SignStyle.ALWAYS, 1, 5],
+            ['50', 1, 2, SignStyle.ALWAYS, 2, 50],
+            ['500', 1, 2, SignStyle.ALWAYS, 3, 500],
+            ['-0', 1, 2, SignStyle.ALWAYS, 2, 0],
+            ['-5', 1, 2, SignStyle.ALWAYS, 2, -5],
+            ['-50', 1, 2, SignStyle.ALWAYS, 3, -50],
+            ['-500', 1, 2, SignStyle.ALWAYS, 4, -500],
+            ['-AAA', 1, 2, SignStyle.ALWAYS, ~1, null],
+            ['+0', 1, 2, SignStyle.ALWAYS, 2, 0],
+            ['+5', 1, 2, SignStyle.ALWAYS, 2, 5],
+            ['+50', 1, 2, SignStyle.ALWAYS, 3, 50],
+            ['+500', 1, 2, SignStyle.ALWAYS, 4, 500],
+            ['+AAA', 1, 2, SignStyle.ALWAYS, ~1, null],
+
+            // exceeds pad
+            ['0', 1, 2, SignStyle.EXCEEDS_PAD, 1, 0],
+            ['5', 1, 2, SignStyle.EXCEEDS_PAD, 1, 5],
+            ['50', 1, 2, SignStyle.EXCEEDS_PAD, 2, 50],
+            ['500', 1, 2, SignStyle.EXCEEDS_PAD, 3, 500],
+            ['-0', 1, 2, SignStyle.EXCEEDS_PAD, 2, 0],
+            ['-5', 1, 2, SignStyle.EXCEEDS_PAD, 2, -5],
+            ['-50', 1, 2, SignStyle.EXCEEDS_PAD, 3, -50],
+            ['-500', 1, 2, SignStyle.EXCEEDS_PAD, 4, -500],
+            ['-AAA', 1, 2, SignStyle.EXCEEDS_PAD, ~1, null],
+            ['+0', 1, 2, SignStyle.EXCEEDS_PAD, 2, 0],
+            ['+5', 1, 2, SignStyle.EXCEEDS_PAD, 2, 5],
+            ['+50', 1, 2, SignStyle.EXCEEDS_PAD, 3, 50],
+            ['+500', 1, 2, SignStyle.EXCEEDS_PAD, 4, 500],
+            ['+AAA', 1, 2, SignStyle.EXCEEDS_PAD, ~1, null]
+        ];
+
+        it('test_parseSignsStrict', () => {
+            for(var i=0; i < provider_parseSignsLenient.length; i++){
+                test_parseSignsLenient.apply(this, provider_parseSignsLenient[i]);
+            }
+        });
+
+        function test_parseSignsLenient(input, min, max, style, parseLen, parseVal){
+            // console.log(input, min, max, style, parseLen, parseVal);
+            var parseContext = new DateTimeParseContext(null, DecimalStyle.STANDARD, IsoChronology.INSTANCE);
+            parseContext.setStrict(false);
+            var pp = new NumberPrinterParser(DAY_OF_MONTH, min, max, style);
+            var newPos = pp.parse(parseContext, input, 0);
+            assertEquals(newPos, parseLen);
+            assertParsed(parseContext, DAY_OF_MONTH, (parseVal != null ? parseVal : null));
+        }
+    });
+
     function assertParsed(context, temporalField, value) {
         if (value == null) {
             assertEquals(context.getParsed(temporalField), null);
