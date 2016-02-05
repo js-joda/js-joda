c16c4078aa6948bcdbac35bbd240f0b0813e605f
Philipp Thürwächter <pithu@thuerwaechter.de>
2016-02-05 12:51:39 +0100

    lint and get rid of comments in minify version

--- webpack.config.js
+++ webpack.config.js
@@ -1,31 +1,37 @@
+var fs = require('fs');
 var path = require('path');
-var webpack = require("webpack");
+var webpack = require('webpack');
 
-var minify = JSON.parse(process.env.DIST_MIN || "0");
+var minify = JSON.parse(process.env.DIST_MIN || '0');
 var sourceMaps = !minify;
 
 module.exports = {
     context: __dirname,
     entry: './src/js-joda.js',
-    devtool: sourceMaps ? "source-map" : "",
+    devtool: sourceMaps ? 'source-map' : '',
     output: {
-        path: __dirname  + "/dist",
+        path: __dirname  + '/dist',
         filename: minify ? 'js-joda.min.js' : 'js-joda.js',
         libraryTarget: minify ? 'var' : 'umd',
         library: 'jsjoda'
     },
     module: {
         loaders: [{
-                loader: 'babel-loader',
-                include: [
-                    path.resolve(__dirname, "src"),
-                    path.resolve(__dirname, "test")
-                ],
-                test: /.js$/
+            loader: 'babel-loader',
+            include: [
+                path.resolve(__dirname, 'src'),
+                path.resolve(__dirname, 'test')
+            ],
+            test: /.js$/
         }]
     },
     plugins: minify ? [
-      new webpack.optimize.UglifyJsPlugin({minimize: true})
+        new webpack.optimize.UglifyJsPlugin({
+            comments: false
+        }),
+        new webpack.BannerPlugin(
+            fs.readFileSync('./src/license-preample.js', 'utf8'),
+            {raw: true}
+        )
     ] : []
 };
-
