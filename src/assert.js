9b3425cab057f114a7320beea25f67ab36c6efc3
Philipp Thürwächter <pithu@thuerwaechter.de>
2016-02-08 15:34:33 +0100

    assert takes 2 arguments as well
    assertion and Error class

--- src/assert.js
+++ src/assert.js
@@ -6,6 +6,11 @@ import {NullPointerException} from './errors';
 
 export function assert(assertion, msg, error) {
     if(!assertion){
+        if(arguments.length === 2){
+            error = msg;
+            msg = null;
+        }
+
         if (error) {
             throw new error(msg);
         } else {
