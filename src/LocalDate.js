4392fbbea2741d566ba146daf77c17fc0b861b92
Philipp Thürwächter <pithu@thuerwaechter.de>
2016-02-16 18:53:25 +0100

    whitespace

--- src/LocalDate.js
+++ src/LocalDate.js
@@ -27,12 +27,11 @@ import {LocalTime} from './LocalTime';
  */
 const  DAYS_PER_CYCLE = 146097;
 
-
-  /**
-   * The number of days from year zero to year 1970.
-   * There are five 400 year cycles from year zero to 2000.
-   * There are 7 leap years from 1970 to 2000.
-   */
+/**
+* The number of days from year zero to year 1970.
+* There are five 400 year cycles from year zero to 2000.
+* There are 7 leap years from 1970 to 2000.
+*/
 const  DAYS_0000_TO_1970 = (DAYS_PER_CYCLE * 5) - (30 * 365 + 7);
 
 /**
