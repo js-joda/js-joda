9e9ee2f873940ed69624a5f44d5c3fa75978f8a8
Philipp Thürwächter <pithu@thuerwaechter.de>
2016-02-19 18:32:25 +0100

    lint

--- karma.conf.js
+++ karma.conf.js
@@ -1,23 +1,23 @@
 module.exports = function(config) {
-  config.set({
-    files: [
-        {pattern: 'test/*Test.js'},
-        {pattern: 'test/**/*Test.js'}
-    ],
-    frameworks: [
-        'mocha',
-        'chai'
-    ],
-    preprocessors: {
-      'test/*.js': ['webpack', 'sourcemap'],
-      'test/**/*.js': ['webpack', 'sourcemap']
-    },
-    webpack: require("./webpack.config.js"),
-    webpackMiddleware: {
-      noInfo: true
-    },
-    reporters: ['progress'],
-    browsers: ['Chrome', 'Firefox', 'PhantomJS'],
-    plugins: [ "karma-*" ]
-  })
+    config.set({
+        files: [
+            {pattern: 'test/*Test.js'},
+            {pattern: 'test/**/*Test.js'}
+        ],
+        frameworks: [
+            'mocha',
+            'chai'
+        ],
+        preprocessors: {
+            'test/*.js': ['webpack', 'sourcemap'],
+            'test/**/*.js': ['webpack', 'sourcemap']
+        },
+        webpack: require('./webpack.config.js'),
+        webpackMiddleware: {
+            noInfo: true
+        },
+        reporters: ['progress'],
+        browsers: ['Chrome', 'Firefox', 'PhantomJS'],
+        plugins: ['karma-*']
+    });
 };
