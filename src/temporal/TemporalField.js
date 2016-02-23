9bbf73f4e3c56acdf847a169ac73fadb20adfb24
Philipp Thürwächter <pithu@thuerwaechter.de>
2016-02-23 10:30:17 +0100

    esdoc

--- src/temporal/TemporalField.js
+++ src/temporal/TemporalField.js
@@ -3,8 +3,8 @@
  * @copyright (c) 2007-present, Stephen Colebourne & Michael Nascimento Santos
  * @license BSD-3-Clause (see LICENSE in the root directory of this source tree)
  */
+/**
+ * @interface
+ */
 export class TemporalField {
-    constructor() {
-        
-    }
 }
