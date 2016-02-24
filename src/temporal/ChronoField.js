6a833cb0e20ace53618d917120775c69f65b2a02
Philipp Thürwächter <pithu@thuerwaechter.de>
2016-02-24 22:32:42 +0100

    docu for static Class props

--- src/temporal/ChronoField.js
+++ src/temporal/ChronoField.js
@@ -19,6 +19,69 @@ import {Year} from '../Year';
  * For example, most non-ISO calendar systems define dates as a year, month and day,
  * just with slightly different rules.
  * The documentation of each field explains how it operates.
+ * 
+ * <h3>Static properties of Class {@link ChronoField}</h3>
+ * 
+ * ChronoField.NANO_OF_SECOND
+ *
+ * ChronoField.NANO_OF_DAY
+ *
+ * ChronoField.MICRO_OF_SECOND
+ *
+ * ChronoField.MICRO_OF_DAY
+ *
+ * ChronoField.MILLI_OF_SECOND
+ *
+ * ChronoField.MILLI_OF_DAY
+ *
+ * ChronoField.SECOND_OF_MINUTE
+ *
+ * ChronoField.SECOND_OF_DAY
+ *
+ * ChronoField.MINUTE_OF_HOUR
+ *
+ * ChronoField.MINUTE_OF_DAY
+ *
+ * ChronoField.HOUR_OF_AMPM
+ *
+ * ChronoField.CLOCK_HOUR_OF_AMPM
+ *
+ * ChronoField.HOUR_OF_DAY
+ *
+ * ChronoField.CLOCK_HOUR_OF_DAY
+ *
+ * ChronoField.AMPM_OF_DAY
+ *
+ * ChronoField.DAY_OF_WEEK
+ *
+ * ChronoField.ALIGNED_DAY_OF_WEEK_IN_MONTH
+ *
+ * ChronoField.ALIGNED_DAY_OF_WEEK_IN_YEAR
+ *
+ * ChronoField.DAY_OF_MONTH
+ *
+ * ChronoField.DAY_OF_YEAR
+ *
+ * ChronoField.EPOCH_DAY
+ *
+ * ChronoField.ALIGNED_WEEK_OF_MONTH
+ *
+ * ChronoField.ALIGNED_WEEK_OF_YEAR
+ *
+ * ChronoField.MONTH_OF_YEAR
+ *
+ * ChronoField.PROLEPTIC_MONTH
+ *
+ * ChronoField.YEAR_OF_ERA
+ *
+ * ChronoField.YEAR
+ *
+ * ChronoField.ERA
+ *
+ * ChronoField.INSTANT_SECONDS
+ *
+ * ChronoField.OFFSET_SECONDS
+ *
  */
 export class ChronoField extends TemporalField {
 
