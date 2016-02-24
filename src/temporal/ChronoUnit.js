74e16fd698e7405de0d64af311276abe7993339d
Philipp Thürwächter <pithu@thuerwaechter.de>
2016-02-24 22:32:49 +0100

    docu for static Class props

--- src/temporal/ChronoUnit.js
+++ src/temporal/ChronoUnit.js
@@ -20,6 +20,121 @@ import {TemporalUnit} from './TemporalUnit';
  * For example, most non-ISO calendar systems define units of years, months and days,
  * just with slightly different rules.
  * The documentation of each unit explains how it operates.
+ *
+ * <h3>Static properties of Class {@link ChronoUnit}</h3>
+ *
+ * ChronoUnit.NANOS
+ *
+ * Unit that represents the concept of a nanosecond, the smallest supported unit of time.
+ * For the ISO calendar system, it is equal to the 1,000,000,000th part of the second unit.
+ *
+ * ChronoUnit.MICROS
+ *
+ * Unit that represents the concept of a microsecond.
+ * For the ISO calendar system, it is equal to the 1,000,000th part of the second unit.
+ *
+ * ChronoUnit.MILLIS
+ *
+ * Unit that represents the concept of a millisecond.
+ * For the ISO calendar system, it is equal to the 1000th part of the second unit.
+ *
+ * ChronoUnit.SECONDS
+ *
+ * Unit that represents the concept of a second.
+ * For the ISO calendar system, it is equal to the second in the SI system
+ * of units, except around a leap-second.
+ *
+ * ChronoUnit.MINUTES
+ *
+ * Unit that represents the concept of a minute.
+ * For the ISO calendar system, it is equal to 60 seconds.
+ *
+ * ChronoUnit.HOURS
+ *
+ * Unit that represents the concept of an hour.
+ * For the ISO calendar system, it is equal to 60 minutes.
+ *
+ * ChronoUnit.HALF_DAYS
+ *
+ * Unit that represents the concept of half a day, as used in AM/PM.
+ * For the ISO calendar system, it is equal to 12 hours.
+ *
+ * ChronoUnit.DAYS
+ *
+ * Unit that represents the concept of a day.
+ * For the ISO calendar system, it is the standard day from midnight to midnight.
+ * The estimated duration of a day is {@code 24 Hours}.
+ * <p>
+ * When used with other calendar systems it must correspond to the day defined by
+ * the rising and setting of the Sun on Earth. It is not required that days begin
+ * at midnight - when converting between calendar systems, the date should be
+ * equivalent at midday.
+ *
+ * ChronoUnit.WEEKS
+ *
+ * Unit that represents the concept of a week.
+ * For the ISO calendar system, it is equal to 7 days.
+ * <p>
+ * When used with other calendar systems it must correspond to an integral number of days.
+ *
+ * ChronoUnit.MONTHS
+ *
+ * Unit that represents the concept of a month.
+ * For the ISO calendar system, the length of the month varies by month-of-year.
+ * The estimated duration of a month is one twelfth of {@code 365.2425 Days}.
+ * <p>
+ * When used with other calendar systems it must correspond to an integral number of days.
+ *
+ * ChronoUnit.YEARS
+ *
+ * Unit that represents the concept of a year.
+ * For the ISO calendar system, it is equal to 12 months.
+ * The estimated duration of a year is {@code 365.2425 Days}.
+ * <p>
+ * When used with other calendar systems it must correspond to an integral number of days
+ * or months roughly equal to a year defined by the passage of the Earth around the Sun.
+ *
+ * ChronoUnit.DECADES
+ *
+ * Unit that represents the concept of a decade.
+ * For the ISO calendar system, it is equal to 10 years.
+ * <p>
+ * When used with other calendar systems it must correspond to an integral number of days
+ * and is normally an integral number of years.
+ *
+ * ChronoUnit.CENTURIES
+ *
+ * Unit that represents the concept of a century.
+ * For the ISO calendar system, it is equal to 100 years.
+ * <p>
+ * When used with other calendar systems it must correspond to an integral number of days
+ * and is normally an integral number of years.
+ *
+ * ChronoUnit.MILLENNIA
+ *
+ * Unit that represents the concept of a millennium.
+ * For the ISO calendar system, it is equal to 1000 years.
+ * <p>
+ * When used with other calendar systems it must correspond to an integral number of days
+ * and is normally an integral number of years.
+ *
+ * ChronoUnit.ERAS
+ *
+ * Unit that represents the concept of an era.
+ * The ISO calendar system doesn't have eras thus it is impossible to add
+ * an era to a date or date-time.
+ * The estimated duration of the era is artificially defined as {Year.MAX_VALUE} + 1.
+ * <p>
+ * When used with other calendar systems there are no restrictions on the unit.
+ *
+ * ChronoUnit.FOREVER
+ *
+ * Artificial unit that represents the concept of forever.
+ * This is primarily used with {@link TemporalField} to represent unbounded fields
+ * such as the year or era.
+ * The estimated duration of the era is artificially defined as the largest duration
+ * supported by {@code Duration}.
+ *
  */
 export class ChronoUnit extends TemporalUnit {
 
@@ -29,6 +144,7 @@ export class ChronoUnit extends TemporalUnit {
      * @param {Duration} estimatedDuration
      */
     constructor (name, estimatedDuration) {
+        super();
         this._name = name;
         this._duration = estimatedDuration;
     }
