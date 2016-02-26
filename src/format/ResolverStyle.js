4efa226c64a71c9d29d9a43c61077ce9537d2567
Philipp Thürwächter <pithu@thuerwaechter.de>
2016-02-26 18:25:12 +0100

    white space and add missing copyright banner

--- src/format/ResolverStyle.js
+++ src/format/ResolverStyle.js
@@ -1,3 +1,11 @@
+/*
+ * @copyright (c) 2016, Philipp Thuerwaechter & Pattrick Hueper
+ * @copyright (c) 2007-present, Stephen Colebourne & Michael Nascimento Santos
+ * @license BSD-3-Clause (see LICENSE in the root directory of this source tree)
+ */
+
+import {Enum} from '../Enum';
+
 /**
  * Enumeration of different ways to resolve dates and times.
  * 
@@ -45,8 +53,6 @@
  * For example, month 15 is treated as being 3 months after month 12.
  *
  */
-import {Enum} from '../Enum';
-
 export class ResolverStyle extends Enum {}
 
 /**
