f4109b4eb890d4dfe9de00835e044354cf115eb2
Philipp Thürwächter <pithu@thuerwaechter.de>
2016-02-26 12:47:43 +0100

    lint and es5

--- benchmark.js
+++ benchmark.js
@@ -5,29 +5,25 @@ var LocalDateTime = require('js-joda').LocalDateTime;
 
 var accessLoops = 50;
 
-
-var testMomentParsePlus = () => {
+function testMomentParsePlus() {
 	var m = moment('2016-02-26T09:29:08.678');
 	var s = '';
 
 	for(var i=0; i<accessLoops; i++){
 		m.add(1, 'months');
 		s += m.toISOString();
-	};
-
+	}
 	// console.log(s);
 }
 
-
-var testJSJodaParsePlus = () => {
+function testJSJodaParsePlus() {
 	var d = LocalDateTime.parse('2016-02-26T09:29:08.678');
 	var s = '';
 
 	for(var i=0; i<accessLoops; i++){
 	        d = d.plusMonths(1);
 	        s += d.toString();
-	};
-	
+	}
 	// console.log(s);
 }
 
@@ -36,11 +32,12 @@ suite
 	.add('moment', testMomentParsePlus)
 	.add('js-joda', testJSJodaParsePlus)
 	.on('complete', function() {
-		console.log('Fastest is ' + this.filter('fastest').map('name'));
-		this.filter('successful').forEach((value) => {
-			console.log('Result for ' + value.name);
-			console.log(JSON.stringify(_.pick(value, ['name', 'hz', 'count']), null, 4));
+        var resultStr = 'Fastest is ' + this.filter('fastest').map('name') + '\n';
+		this.filter('successful').forEach(function (value) {
+			resultStr += 'Result for ' + value.name + '\n';
+            resultStr += JSON.stringify(_.pick(value, ['name', 'hz', 'count']), null, 4) + '\n';
 		});
+        console.log(resultStr)
 	})
 	.run();
 
