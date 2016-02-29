0ab8e8c78a36b63f0d88c2ce1cfb8857ec82643b
Philipp Thürwächter <pithu@thuerwaechter.de>
2016-02-29 15:00:34 +0100

    documentation copied for temporal interfaces

--- src/temporal/TemporalUnit.js
+++ src/temporal/TemporalUnit.js
@@ -5,7 +5,22 @@
  */
 
 /**
- * see {@link ChronoUnit} for a description
+ * A unit of date-time, such as Days or Hours.
+ * <p>
+ * Measurement of time is built on units, such as years, months, days, hours, minutes and seconds.
+ * Implementations of this interface represent those units.
+ * <p>
+ * An instance of this interface represents the unit itself, rather than an amount of the unit.
+ * See {@link Period} for a class that represents an amount in terms of the common units.
+ * <p>
+ * The most commonly used units are defined in {@link ChronoUnit}.
+ * Further units are supplied in {@link IsoFields}.
+ * Units can also be written by application code by implementing this interface.
+ * <p>
+ * The unit works using double dispatch. Client code calls methods on a date-time like
+ * {@code LocalDateTime} which check if the unit is a {@code ChronoUnit}.
+ * If it is, then the date-time must handle it.
+ * Otherwise, the method call is re-dispatched to the matching method in this interface.
  *
  * @interface
  */
