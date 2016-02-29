518e695b9428253a47732163161821f5edc46767
Philipp Thürwächter <pithu@thuerwaechter.de>
2016-02-29 10:30:59 +0100

    extend Errors the old style
    solve some issues with ie9, probably caused by not calling the super class constructor.
    solving https://github.com/pithu/js-joda/issues/15

--- src/errors.js
+++ src/errors.js
@@ -2,54 +2,45 @@
  * @copyright (c) 2016, Philipp Thuerwaechter & Pattrick Hueper
  * @license BSD-3-Clause (see LICENSE in the root directory of this source tree)
  */
-import ExtendableError from 'es6-error';
 
-export class DateTimeException extends ExtendableError {
-    constructor(message = 'DateTimeException', cause = null) {
-        let msg = message;
-        if (cause !== null && cause instanceof Error) {
-            msg += '\n-------\nCaused by: ' + cause.stack + '\n-------\n';
+function createErrorType(name, init, superErrorClass = Error) {
+    function E(message) {
+        if (!Error.captureStackTrace){
+            this.stack = (new Error()).stack;
+        } else {
+            Error.captureStackTrace(this, this.constructor);
         }
-        super(msg);
+        this.message = message;
+        init && init.apply(this, arguments);
+
     }
+    E.prototype = new superErrorClass();
+    E.prototype.name = name;
+    E.prototype.constructor = E;
+    return E;
 }
 
-export class DateTimeParseException extends ExtendableError {
-    constructor(message = 'DateTimeParseException', text = '', index = 0, cause = null) {
-        let msg = message + ': ' + text + ', at index: ' + index;
-        if (cause !== null && cause instanceof Error) {
-            msg += '\n-------\nCaused by: ' + cause.stack + '\n-------\n';
-        }
-        super(msg);
+export var DateTimeException = createErrorType('DateTimeException', messageWithCause);
+export var DateTimeParseException = createErrorType('DateTimeParseException', messageForDateTimeParseException);
+export var UnsupportedTemporalTypeException = createErrorType('UnsupportedTemporalTypeException', null, DateTimeException);
+export var ArithmeticException = createErrorType('ArithmeticException');
+export var IllegalArgumentException = createErrorType('IllegalArgumentException');
+export var IllegalStateException = createErrorType('IllegalStateException');
+export var NullPointerException = createErrorType('NullPointerException');
+
+function messageWithCause(message, cause = null) {
+    let msg = message || this.name;
+    if (cause !== null && cause instanceof Error) {
+        msg += '\n-------\nCaused by: ' + cause.stack + '\n-------\n';
     }
+    this.message = msg;
 }
 
-export class UnsupportedTemporalTypeException extends DateTimeException {
-    constructor(message = 'UnsupportedTemporalTypeException') {
-        super(message);
+function messageForDateTimeParseException(message, text = '', index = 0, cause = null) {
+    let msg = message || this.name;
+    msg += ': ' + text + ', at index: ' + index;
+    if (cause !== null && cause instanceof Error) {
+        msg += '\n-------\nCaused by: ' + cause.stack + '\n-------\n';
     }
-}
-
-export class ArithmeticException extends ExtendableError {
-    constructor(message = 'ArithmeticException') {
-        super(message);
-    }
-}
-
-export class IllegalArgumentException extends ExtendableError {
-    constructor(message = 'IllegalArgumentException') {
-        super(message);
-    }
-}
-
-export class IllegalStateException extends ExtendableError {
-    constructor(message = 'IllegalStateException') {
-        super(message);
-    }
-}
-
-export class NullPointerException extends ExtendableError {
-    constructor(message = 'NullPointerException') {
-        super(message);
-    }
+    this.message = msg;
 }
