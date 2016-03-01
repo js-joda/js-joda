e3a6001a9de57fb995310f4a93b3257348eae2ef
Philipp Thürwächter <pithu@thuerwaechter.de>
2016-03-01 14:15:07 +0100

    whitespace

--- src/js-joda.js
+++ src/js-joda.js
@@ -2,6 +2,7 @@
  * @copyright (c) 2016, Philipp Thuerwaechter & Pattrick Hueper
  * @license BSD-3-Clause (see LICENSE in the root directory of this source tree)
  */
+
 export { Clock } from './Clock';
 export { DateTimeException, DateTimeParseException } from './errors';
 export { DayOfWeek } from './DayOfWeek';
