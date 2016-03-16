fad36bf20e95e63cf30230e854e0ace12bc440b0
Philipp Thürwächter <pithu@thuerwaechter.de>
2016-03-16 16:30:51 +0100

    add OffsetIdPrinterParser with reference tests for print

--- src/format/DateTimeFormatterBuilder.js
+++ src/format/DateTimeFormatterBuilder.js
@@ -402,6 +402,19 @@ export class DateTimeFormatterBuilder {
     }
 
 
+    /**
+     * Appends the zone offset, such as '+01:00', to the formatter.
+     * <p>
+     * This appends an instruction to print/parse the offset ID to the builder.
+     * This is equivalent to calling {@code appendOffset("HH:MM:ss", "Z")}.
+     *
+     * @return {DateTimeFormatterBuilder} this, for chaining, not null
+     */
+    appendOffsetId() {
+        this._appendInternal(OffsetIdPrinterParser.INSTANCE_ID);
+        return this;
+    }
+
     //-----------------------------------------------------------------------
     /**
      * Mark the start of an optional section.
@@ -1275,6 +1288,170 @@ class InstantPrinterParser  {
     }
 }
 
+//-----------------------------------------------------------------------
+const PATTERNS = [
+    '+HH', '+HHmm', '+HH:mm', '+HHMM', '+HH:MM', '+HHMMss', '+HH:MM:ss', '+HHMMSS', '+HH:MM:SS'
+];
+/**
+ * Prints or parses an offset ID.
+ */
+class OffsetIdPrinterParser  {
+
+    /**
+     * Constructor.
+     *
+     * @param {string} noOffsetText  the text to use for UTC, not null
+     * @param {string} pattern  the pattern
+     */
+    constructor(noOffsetText, pattern) {
+        requireNonNull(noOffsetText, 'noOffsetText');
+        requireNonNull(pattern, 'pattern');
+        this.noOffsetText = noOffsetText;
+        this.type = this._checkPattern(pattern);
+    }
+
+    /**
+     * @param {String} pattern
+     * @return {number}
+     */
+    _checkPattern(pattern) {
+        for (let i = 0; i < PATTERNS.length; i++) {
+            if (PATTERNS[i] === pattern) {
+                return i;
+            }
+        }
+        throw new IllegalArgumentException('Invalid zone offset pattern: ' + pattern);
+    }
+
+    /**
+     * @param {DateTimePrintContext} context
+     * @param {StringBuilder} buf
+     * @return {boolean} 
+     */
+    print(context, buf) {
+        var offsetSecs = context.getValue(ChronoField.OFFSET_SECONDS);
+        if (offsetSecs == null) {
+            return false;
+        }
+        var totalSecs = MathUtil.safeToInt(offsetSecs);
+        if (totalSecs === 0) {
+            buf.append(this.noOffsetText);
+        } else {
+            var absHours = Math.abs(MathUtil.intMod(MathUtil.intDiv(totalSecs, 3600), 100));  // anything larger than 99 silently dropped
+            var absMinutes = Math.abs(MathUtil.intMod(MathUtil.intDiv(totalSecs, 60), 60));
+            var absSeconds = Math.abs(MathUtil.intMod(totalSecs, 60));
+            var bufPos = buf.length();
+            var output = absHours;
+            buf.append(totalSecs < 0 ? '-' : '+')
+                .appendChar((MathUtil.intDiv(absHours, 10) + '0')).appendChar(MathUtil.intMod(absHours, 10) + '0');
+            if (this.type >= 3 || (this.type >= 1 && absMinutes > 0)) {
+                buf.append((this.type % 2) === 0 ? ':' : '')
+                    .appendChar((MathUtil.intDiv(absMinutes, 10) + '0')).appendChar((absMinutes % 10 + '0'));
+                output += absMinutes;
+                if (this.type >= 7 || (this.type >= 5 && absSeconds > 0)) {
+                    buf.append((this.type % 2) === 0 ? ':' : '')
+                        .appendChar((MathUtil.intDiv(absSeconds, 10) + '0')).appendChar((absSeconds % 10 + '0'));
+                    output += absSeconds;
+                }
+            }
+            if (output === 0) {
+                buf.setLength(bufPos);
+                buf.append(this.noOffsetText);
+            }
+        }
+        return true;
+    }
+
+    /**
+     * @param {DateTimeParseContext} context
+     * @param {String} text
+     * @param {number} position
+     * @return {number}
+     */
+    parse(context, text, position) {
+        var length = text.length;
+        var noOffsetLen = this.noOffsetText.length;
+        if (noOffsetLen === 0) {
+            if (position === length) {
+                return context.setParsedField(ChronoField.OFFSET_SECONDS, 0, position, position);
+            }
+        } else {
+            if (position === length) {
+                return ~position;
+            }
+            if (context.subSequenceEquals(text, position, this.noOffsetText, 0, noOffsetLen)) {
+                return context.setParsedField(ChronoField.OFFSET_SECONDS, 0, position, position + noOffsetLen);
+            }
+        }
+
+        // parse normal plus/minus offset
+        var sign = text[position];  // IOOBE if invalid position
+        if (sign === '+' || sign === '-') {
+            // starts
+            var negative = (sign === '-' ? -1 : 1);
+            var array = [];
+            array[0] = position + 1;
+            if ((this._parseNumber(array, 1, text, true) ||
+                    this._parseNumber(array, 2, text, this.type >=3) ||
+                    this._parseNumber(array, 3, text, false)) === false) {
+                // success
+                var offsetSecs = negative * (array[1] * 3600 + array[2] * 60 + array[3]);
+                return context.setParsedField(ChronoField.OFFSET_SECONDS, offsetSecs, position, array[0]);
+            }
+        }
+        // handle special case of empty no offset text
+        if (noOffsetLen === 0) {
+            return context.setParsedField(ChronoField.OFFSET_SECONDS, 0, position, position + noOffsetLen);
+        }
+        return ~position;
+    }
+
+    /**
+     * Parse a two digit zero-prefixed number.
+     *
+     * @param {number[]} array  the array of parsed data, 0=pos,1=hours,2=mins,3=secs, not null
+     * @param {number} arrayIndex  the index to parse the value into
+     * @param {string} parseText  the offset ID, not null
+     * @param {boolean} required  whether this number is required
+     * @return {boolean} true if an error occurred
+     */
+    parseNumber(array, arrayIndex, parseText, required) {
+        if ((this.type + 3) / 2 < arrayIndex) {
+            return false;  // ignore seconds/minutes
+        }
+        var pos = array[0];
+        if ((this.type % 2) === 0 && arrayIndex > 1) {
+            if (pos + 1 > parseText.length || parseText[pos] !== ':') {
+                return required;
+            }
+            pos++;
+        }
+        if (pos + 2 > parseText.length) {
+            return required;
+        }
+        var ch1 = parseText[pos++];
+        var ch2 = parseText[pos++];
+        if (ch1 < '0' || ch1 > '9' || ch2 < '0' || ch2 > '9') {
+            return required;
+        }
+        var value = (ch1 - 48) * 10 + (ch2 - 48);
+        if (value < 0 || value > 59) {
+            return required;
+        }
+        array[arrayIndex] = value;
+        array[0] = pos;
+        return false;
+    }
+
+
+    toString() {
+        var converted = this.noOffsetText.replace('\'', '\'\'');
+        return 'Offset(' + PATTERNS[this.type] + ',"' + converted + '")';
+    }
+}
+
+OffsetIdPrinterParser.INSTANCE_ID = new OffsetIdPrinterParser('Z', '+HH:MM:ss');
+
 DateTimeFormatterBuilder.CompositePrinterParser = CompositePrinterParser;
 DateTimeFormatterBuilder.PadPrinterParserDecorator = PadPrinterParserDecorator;
 DateTimeFormatterBuilder.SettingsParser = SettingsParser;
@@ -1282,3 +1459,4 @@ DateTimeFormatterBuilder.CharLiteralPrinterParser = StringLiteralPrinterParser;
 DateTimeFormatterBuilder.StringLiteralPrinterParser = StringLiteralPrinterParser;
 DateTimeFormatterBuilder.NumberPrinterParser = NumberPrinterParser;
 DateTimeFormatterBuilder.FractionPrinterParser = FractionPrinterParser;
+DateTimeFormatterBuilder.OffsetIdPrinterParser = OffsetIdPrinterParser;
---
+++ test/reference/format/ZoneOffsetPrinterTest.js
@@ -0,0 +1,154 @@
+/*
+ * @copyright (c) 2016, Philipp Thuerwaechter & Pattrick Hueper                 
+ * @copyright (c) 2007-present, Stephen Colebourne & Michael Nascimento Santos  
+ * @license BSD-3-Clause (see LICENSE in the root directory of this source tree)
+ */
+
+import {expect} from 'chai';
+
+import '../../_init';
+
+import {dataProviderTest, assertEquals} from '../../testUtils';
+
+import {DateTimeException} from '../../../src/errors';
+
+import {DateTimeBuilder} from '../../../src/format/DateTimeBuilder';
+import {DateTimeFormatterBuilder} from '../../../src/format/DateTimeFormatterBuilder';
+import {DateTimePrintContext} from '../../../src/format/DateTimePrintContext';
+import {DecimalStyle} from '../../../src/format/DecimalStyle';
+import {StringBuilder} from '../../../src/format/StringBuilder';
+import {LocalDate} from '../../../src/LocalDate';
+import {ZoneOffset} from '../../../src/ZoneOffset';
+
+import {ChronoField} from '../../../src/temporal/ChronoField';
+
+import {EMPTY} from '../temporal/Empty';
+
+var OffsetIdPrinterParser = DateTimeFormatterBuilder.OffsetIdPrinterParser;
+
+describe('org.threeten.bp.format.TestZoneOffsetPrinter', ()=>{
+
+    var OFFSET_0130 = null;
+    var printEmptyContext, printContext;
+    var buf;
+
+    beforeEach(() => {
+        OFFSET_0130 = ZoneOffset.ofHoursMinutes(1,30); // '+01:30');
+        init();
+    });
+
+    function init(){
+        printEmptyContext = new DateTimePrintContext(new EMPTY(), null, DecimalStyle.STANDARD);
+        var d = LocalDate.of(2011, 6, 30);
+        printContext = new DateTimePrintContext(d, null, DecimalStyle.STANDARD);
+        buf = new StringBuilder();
+    }
+
+    // @DataProvider(name="offsets")
+    function provider_offsets() {
+        return [
+            ['+HH', 'NO-OFFSET', ZoneOffset.UTC],
+            ['+HH', '+01', ZoneOffset.ofHours(1)],
+            ['+HH', '-01', ZoneOffset.ofHours(-1)],
+
+            ['+HHMM', 'NO-OFFSET', ZoneOffset.UTC],
+            ['+HHMM', '+0102', ZoneOffset.ofHoursMinutes(1, 2)],
+            ['+HHMM', '-0102', ZoneOffset.ofHoursMinutes(-1, -2)],
+
+            ['+HH:MM', 'NO-OFFSET', ZoneOffset.UTC],
+            ['+HH:MM', '+01:02', ZoneOffset.ofHoursMinutes(1, 2)],
+            ['+HH:MM', '-01:02', ZoneOffset.ofHoursMinutes(-1, -2)],
+
+            ['+HHMMss', 'NO-OFFSET', ZoneOffset.UTC],
+            ['+HHMMss', '+0100', ZoneOffset.ofHoursMinutesSeconds(1, 0, 0)],
+            ['+HHMMss', '+0102', ZoneOffset.ofHoursMinutesSeconds(1, 2, 0)],
+            ['+HHMMss', '+0159', ZoneOffset.ofHoursMinutesSeconds(1, 59, 0)],
+            ['+HHMMss', '+0200', ZoneOffset.ofHoursMinutesSeconds(2, 0, 0)],
+            ['+HHMMss', '+1800', ZoneOffset.ofHoursMinutesSeconds(18, 0, 0)],
+            ['+HHMMss', '+010215', ZoneOffset.ofHoursMinutesSeconds(1, 2, 15)],
+            ['+HHMMss', '-0100', ZoneOffset.ofHoursMinutesSeconds(-1, 0, 0)],
+            ['+HHMMss', '-0200', ZoneOffset.ofHoursMinutesSeconds(-2, 0, 0)],
+            ['+HHMMss', '-1800', ZoneOffset.ofHoursMinutesSeconds(-18, 0, 0)],
+
+            ['+HHMMss', 'NO-OFFSET', ZoneOffset.UTC],
+            ['+HHMMss', '+0100', ZoneOffset.ofHoursMinutesSeconds(1, 0, 0)],
+            ['+HHMMss', '+010203', ZoneOffset.ofHoursMinutesSeconds(1, 2, 3)],
+            ['+HHMMss', '+015959', ZoneOffset.ofHoursMinutesSeconds(1, 59, 59)],
+            ['+HHMMss', '+0200', ZoneOffset.ofHoursMinutesSeconds(2, 0, 0)],
+            ['+HHMMss', '+1800', ZoneOffset.ofHoursMinutesSeconds(18, 0, 0)],
+            ['+HHMMss', '-0100', ZoneOffset.ofHoursMinutesSeconds(-1, 0, 0)],
+            ['+HHMMss', '-0200', ZoneOffset.ofHoursMinutesSeconds(-2, 0, 0)],
+            ['+HHMMss', '-1800', ZoneOffset.ofHoursMinutesSeconds(-18, 0, 0)],
+
+            ['+HH:MM:ss', 'NO-OFFSET', ZoneOffset.UTC],
+            ['+HH:MM:ss', '+01:00', ZoneOffset.ofHoursMinutesSeconds(1, 0, 0)],
+            ['+HH:MM:ss', '+01:02', ZoneOffset.ofHoursMinutesSeconds(1, 2, 0)],
+            ['+HH:MM:ss', '+01:59', ZoneOffset.ofHoursMinutesSeconds(1, 59, 0)],
+            ['+HH:MM:ss', '+02:00', ZoneOffset.ofHoursMinutesSeconds(2, 0, 0)],
+            ['+HH:MM:ss', '+18:00', ZoneOffset.ofHoursMinutesSeconds(18, 0, 0)],
+            ['+HH:MM:ss', '+01:02:15', ZoneOffset.ofHoursMinutesSeconds(1, 2, 15)],
+            ['+HH:MM:ss', '-01:00', ZoneOffset.ofHoursMinutesSeconds(-1, 0, 0)],
+            ['+HH:MM:ss', '-02:00', ZoneOffset.ofHoursMinutesSeconds(-2, 0, 0)],
+            ['+HH:MM:ss', '-18:00', ZoneOffset.ofHoursMinutesSeconds(-18, 0, 0)],
+
+            ['+HH:MM:ss', 'NO-OFFSET', ZoneOffset.UTC],
+            ['+HH:MM:ss', '+01:00', ZoneOffset.ofHoursMinutesSeconds(1, 0, 0)],
+            ['+HH:MM:ss', '+01:02:03', ZoneOffset.ofHoursMinutesSeconds(1, 2, 3)],
+            ['+HH:MM:ss', '+01:59:59', ZoneOffset.ofHoursMinutesSeconds(1, 59, 59)],
+            ['+HH:MM:ss', '+02:00', ZoneOffset.ofHoursMinutesSeconds(2, 0, 0)],
+            ['+HH:MM:ss', '+18:00', ZoneOffset.ofHoursMinutesSeconds(18, 0, 0)],
+            ['+HH:MM:ss', '-01:00', ZoneOffset.ofHoursMinutesSeconds(-1, 0, 0)],
+            ['+HH:MM:ss', '-02:00', ZoneOffset.ofHoursMinutesSeconds(-2, 0, 0)],
+            ['+HH:MM:ss', '-18:00', ZoneOffset.ofHoursMinutesSeconds(-18, 0, 0)],
+
+            ['+HHMMSS', 'NO-OFFSET', ZoneOffset.UTC],
+            ['+HHMMSS', '+010203', ZoneOffset.ofHoursMinutesSeconds(1, 2, 3)],
+            ['+HHMMSS', '-010203', ZoneOffset.ofHoursMinutesSeconds(-1, -2, -3)],
+            ['+HHMMSS', '+010200', ZoneOffset.ofHoursMinutesSeconds(1, 2, 0)],
+            ['+HHMMSS', '-010200', ZoneOffset.ofHoursMinutesSeconds(-1, -2, 0)],
+
+            ['+HH:MM:SS', 'NO-OFFSET', ZoneOffset.UTC],
+            ['+HH:MM:SS', '+01:02:03', ZoneOffset.ofHoursMinutesSeconds(1, 2, 3)],
+            ['+HH:MM:SS', '-01:02:03', ZoneOffset.ofHoursMinutesSeconds(-1, -2, -3)],
+            ['+HH:MM:SS', '+01:02:00', ZoneOffset.ofHoursMinutesSeconds(1, 2, 0)],
+            ['+HH:MM:SS', '-01:02:00', ZoneOffset.ofHoursMinutesSeconds(-1, -2, 0)]
+        ];
+    }
+    
+    // @Test(dataProvider="offsets")
+    it('test_print', function () {
+        dataProviderTest(provider_offsets, (pattern, expected, offset) => {
+            init();
+            buf.append('EXISTING');
+            printContext.setDateTime(DateTimeBuilder.create(ChronoField.OFFSET_SECONDS, offset.totalSeconds()));
+            var pp = new OffsetIdPrinterParser('NO-OFFSET', pattern);
+            pp.print(printContext, buf);
+            assertEquals(buf.toString(), 'EXISTING' + expected);
+        });
+    });    
+    
+    // @Test(dataProvider="offsets")
+    it('test_toString', function () {
+        dataProviderTest(provider_offsets, (pattern) => {
+            init();
+            var pp = new OffsetIdPrinterParser('NO-OFFSET', pattern);
+            assertEquals(pp.toString(), 'Offset(' + pattern + ',"NO-OFFSET")');
+        });
+    });
+
+    it('test_print_emptyCalendrical()', () => {
+        expect(()=>{
+            var pp = new OffsetIdPrinterParser('Z', '+HH:MM:ss');
+            pp.print(printEmptyContext, buf);
+        }).to.throw(DateTimeException);
+    });
+
+    it('test_print_emptyAppendable()', () => {
+        printContext.setDateTime(DateTimeBuilder.create(ChronoField.OFFSET_SECONDS, OFFSET_0130.totalSeconds()));
+        var pp = new OffsetIdPrinterParser('Z', '+HH:MM:ss');
+        pp.print(printContext, buf);
+        assertEquals(buf.toString(), '+01:30');
+    });
+
+});
+
