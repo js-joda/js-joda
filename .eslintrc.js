536d7f2684878cc0784bf3b37eb9b944b7bc1711
Pattrick Hueper <phueper@hueper.net>
2016-03-16 10:15:55 +0100

    eslint config from yml to js ... and updated to eslint 2.0 format (cherry picked from commit f5f9755)

---
+++ .eslintrc.js
@@ -0,0 +1,39 @@
+module.exports = {
+    "extends": "eslint:recommended",
+    "parserOptions": {
+        "ecmaVersion": 6,
+        "sourceType": "module",
+        "forOf": false
+    },
+    "rules": {
+        "indent": [
+            2,
+            4,
+            {
+                "SwitchCase": 1
+            }
+        ],
+        "quotes": [
+            2,
+            "single"
+        ],
+        "linebreak-style": [
+            2,
+            "unix"
+        ],
+        "semi": [
+            2,
+            "always"
+        ],
+        "eqeqeq": [
+            2,
+            "smart"
+        ]
+    },
+    "env": {
+        "es6": false,
+        "browser": true,
+        "node": true,
+        "mocha": true
+    }
+};
