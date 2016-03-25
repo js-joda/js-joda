2cec8f01149351a837745368383ef2a343866805
Pattrick Hueper <phueper@hueper.net>
2016-03-25 14:56:52 +0100

    Year.now() and tests

--- src/Year.js
+++ src/Year.js
@@ -3,6 +3,15 @@
  * @copyright (c) 2007-present, Stephen Colebourne & Michael Nascimento Santos
  * @license BSD-3-Clause (see LICENSE in the root directory of this source tree)
  */
+
+import {requireNonNull, requireInstance} from './assert';
+
+import {ChronoField} from './temporal/ChronoField';
+import {Clock} from './Clock';
+import {LocalDate} from './LocalDate';
+import {Temporal} from './temporal/Temporal';
+import {ZoneId} from './ZoneId';
+
 /**
  * A year in the ISO-8601 calendar system, such as {@code 2007}.
  * <p>
@@ -29,26 +38,138 @@
  *
  * <h3>Static properties of Class {@link LocalDate}</h3>
  *
- * Year.MIN_VALUE = -999.999;
+ * YearConstants.MIN_VALUE = -999.999;
  *
  * The minimum supported year. Theoretically the minimum could be -28.542.4812 years in javascript.
  * approx LocalDateTime.ofEpochSecond(Number.MIN_SAFE_INTEGER, 0, ZoneOffset.UTC).year()
  *
- * Year.MAX_VALUE = 999.999;
+ * YearConstants.MAX_VALUE = 999.999;
  *
  * The maximum supported year. Theoretically the maximum could be 285.428.751 years in javascript.
  * approx LocalDateTime.ofEpochSecond(Number.MAX_SAFE_INTEGER, 0, ZoneOffset.UTC).year()
  *
  */
-export class Year {}
+export class Year extends Temporal {
+
+    /**
+     *
+     * @param {number} value
+     */
+    constructor(value) {
+        super();
+        this._value = value;
+    }
+
+    /**
+     *
+     * @return {number} gets the value
+     */
+    value() {
+        return this._value;
+    }
+
+    /**
+     * to handle function overriding this function accepts zero or one arguments, checks their type and delegates to the appropriate function
+     *
+     * @return {Year}
+     */
+    static now(arg = undefined) {
+        if (arg === undefined) {
+            return Year._now();
+        } else if (arg instanceof ZoneId) {
+            return Year._nowZoneId(arg);
+        } else {
+            return Year._nowClock(arg);
+        }
+    }
+    
+    /**
+     * Obtains the current year from the system clock in the default time-zone.
+     * <p>
+     * This will query the {@link Clock#systemDefaultZone() system clock} in the default
+     * time-zone to obtain the current year.
+     * <p>
+     * Using this method will prevent the ability to use an alternate clock for testing
+     * because the clock is hard-coded.
+     *
+     * @return {Year} the current year using the system clock and default time-zone, not null
+     */
+    static _now() {
+        return Year._nowClock(Clock.systemDefaultZone());
+    }
+
+    /**
+     * Obtains the current year from the system clock in the specified time-zone.
+     * <p>
+     * This will query the {@link Clock#system(ZoneId) system clock} to obtain the current year.
+     * Specifying the time-zone avoids dependence on the default time-zone.
+     * <p>
+     * Using this method will prevent the ability to use an alternate clock for testing
+     * because the clock is hard-coded.
+     *
+     * @param {ZoneId} zone  the zone ID to use, not null
+     * @return {Year} the current year using the system clock, not null
+     */
+    static _nowZoneId(zone) {
+        requireNonNull(zone, 'zone');
+        requireInstance(zone, ZoneId, 'zone');
+        return Year._nowClock(Clock.system(zone));
+    }
+
+    /**
+     * Obtains the current year from the specified clock.
+     * <p>
+     * This will query the specified clock to obtain the current year.
+     * Using this method allows the use of an alternate clock for testing.
+     * The alternate clock may be introduced using {@link Clock dependency injection}.
+     *
+     * @param {Clock} clock  the clock to use, not null
+     * @return {Year} the current year, not null
+     */
+    static _nowClock(clock) {
+        requireNonNull(clock, 'clock');
+        requireInstance(clock, Clock, 'clock');
+        let now = LocalDate.now(clock);  // called once
+        return Year.of(now.year());
+    }
+    /**
+     * Obtains an instance of {@code Year}.
+     * <p>
+     * This method accepts a year value from the proleptic ISO calendar system.
+     * <p>
+     * The year 2AD/CE is represented by 2.<br>
+     * The year 1AD/CE is represented by 1.<br>
+     * The year 1BC/BCE is represented by 0.<br>
+     * The year 2BC/BCE is represented by -1.<br>
+     *
+     * @param {Number} isoYear  the ISO proleptic year to represent, from {@code MIN_VALUE} to {@code MAX_VALUE}
+     * @return {Year} the year, not null
+     * @throws DateTimeException if the field is invalid
+     */
+    static of(isoYear) {
+        ChronoField.YEAR.checkValidValue(isoYear);
+        return new Year(isoYear);
+    }
+
+    /**
+     * Checks if this year is equal to the specified {@link Year}.
+     * <p>
+     * The comparison is based on the value
+     *
+     * @param {*} otherYear - the other year, null returns false
+     * @return {boolean} true if the other duration is equal to this one
+     */
+    equals(otherYear) {
+        if (this === otherYear) {
+            return true;
+        }
+        if (otherYear instanceof Year) {
+            return this.value() === otherYear.value();
+        }
+        return false;
+    }
+}
 
 export function _init() {
-    /**
-     * The minimum supported year
-     */
-    Year.MIN_VALUE = -999999;
-    /**
-     * The maximum supported year
-     */
-    Year.MAX_VALUE = 999999;
+    /* NOTE: MIN_VALUE/MAX_VALUE moved to YearConstants */
 }
---
+++ test/reference/YearTest.js
@@ -0,0 +1,30 @@
+/*
+ * @copyright (c) 2016, Philipp Thuerwaechter & Pattrick Hueper
+ * @license BSD-3-Clause (see LICENSE.md in the root directory of this source tree)
+ */
+import {expect} from 'chai';
+
+import '../_init';
+
+import {Year} from '../../src/Year';
+import {Clock} from '../../src/Clock';
+
+describe('org.threeten.bp.temporal.TestYear', () => {
+    const TEST_2008 = Year.of(2008);
+
+    //-----------------------------------------------------------------------
+    // now()
+    //-----------------------------------------------------------------------
+    it('now', () => {
+        let expected = Year.now(Clock.systemDefaultZone());
+        let test = Year.now();
+        for (let i = 0; i < 100; i++) {
+            if (expected.equals(test)) {
+                return;
+            }
+            expected = Year.now(Clock.systemDefaultZone());
+            test = Year.now();
+        }
+        expect(test).to.eql(expected);
+    });
+});
