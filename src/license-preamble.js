d52a9d93d5ed33dd6979f40e1a8f1f3cb1dd89f7
Philipp Thürwächter <pithu@thuerwaechter.de>
2016-08-26 07:33:19 +0200

    add contributors to license preamble

--- src/license-preamble.js
+++ src/license-preamble.js
@@ -1,3 +1,3 @@
-//! @copyright (c) 2016, Philipp Thuerwaechter & Pattrick Hueper
+//! @copyright (c) 2015-2016, Philipp Thuerwaechter, Pattrick Hueper, js-joda contributors
 //! @copyright (c) 2007-present, Stephen Colebourne & Michael Nascimento Santos
 //! @license BSD-3-Clause (see LICENSE in the root directory of this source tree)
