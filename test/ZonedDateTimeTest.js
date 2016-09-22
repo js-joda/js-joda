8c4a0fcdca44d86d7a22f33082dab5f7b13e22f5
Philipp Thürwächter <pithu@thuerwaechter.de>
2016-09-22 12:32:56 +0200

    Extend test for several timezones

--- test/ZonedDateTimeTest.js
+++ test/ZonedDateTimeTest.js
@@ -4,26 +4,46 @@
  */
 
 import './_init';
+import {dataProviderTest} from './testUtils';
 
 import {expect} from 'chai';
 
 import {LocalDateTime} from '../src/LocalDateTime';
 import {ZonedDateTime} from '../src/ZonedDateTime';
+import {ZoneOffset} from '../src/ZoneOffset';
 import {SystemDefaultZoneId} from '../src/zone/SystemDefaultZoneId';
+
+import {CurrentStandardZoneEasternTime} from './zone/CurrentStandardZone';
+import {CurrentStandardZoneCEST} from './zone/CurrentStandardZone';
 
 /**
  * js-joda tests for use cases that are missing in the treeten bp reference tests
  */
 describe('ZonedDateTime', () => {
+
     const TEST_LOCAL_2016_06_30_11_30_59_500 = LocalDateTime.of(2016, 6, 30, 11, 30, 59, 500);
     const SYSTEM_DEFAULT_ZONE = new SystemDefaultZoneId();
+    const EDT_EST = new CurrentStandardZoneEasternTime();
+    const CET_CEST = new CurrentStandardZoneCEST();
 
     describe('ofLocal', () => {
 
-        it('should point to the same point in the timeline with the SystemDefaultTimeZone as the UTC Zone', () => {
-            let zdt = ZonedDateTime.ofLocal(TEST_LOCAL_2016_06_30_11_30_59_500, SYSTEM_DEFAULT_ZONE);
-            let utcSameInstant = ZonedDateTime.parse('2016-06-30T11:30:59.000000500Z');
-            expect(utcSameInstant.isEqual(zdt));
+        it('should point to the same point in the timeline for the given zoneIds', () => {
+
+            const ofLocalTestZones = () => {
+                return [
+                    SYSTEM_DEFAULT_ZONE,
+                    ZoneOffset.UTC,
+                    EDT_EST,
+                    CET_CEST
+                ];
+            };
+
+            dataProviderTest(ofLocalTestZones, (zone) => {
+                let zdt = ZonedDateTime.ofLocal(TEST_LOCAL_2016_06_30_11_30_59_500, zone);
+                let utcSameInstant = ZonedDateTime.parse('2016-06-30T11:30:59.000000500Z');
+                expect(utcSameInstant.isEqual(zdt));
+            });
         });
 
     });
