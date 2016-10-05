059ac16b952af2f4712be8117371e468d47b380d
Philipp Thürwächter <pithu@thuerwaechter.de>
2016-10-05 21:45:19 +0200

    Add test abstract ZoneId methods

---
+++ test/ZoneIdTest.js
@@ -0,0 +1,34 @@
+/*
+ * @copyright (c) 2016, Philipp Thürwächter, Pattrick Hüper
+ * @license BSD-3-Clause (see LICENSE in the root directory of this source tree)
+ */
+
+import './_init';
+
+import {expect} from 'chai';
+
+import {ZoneId} from '../src/ZoneId';
+
+describe('ZoneId', function () {
+
+    describe('abstract methods', function () {
+
+        it('should fail for id()', function () {
+            let abstractZomeId  = new ZoneId();
+
+            expect(()=>{
+                abstractZomeId.id();
+            }).to.throw(TypeError);
+        });
+
+        it('should fail for rules()', function () {
+            let abstractZomeId  = new ZoneId();
+
+            expect(()=>{
+                abstractZomeId.rules();
+            }).to.throw(TypeError);
+        });
+
+    });
+
+});
