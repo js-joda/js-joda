bf7ecc960eb2e195e29f2e29ac04862e7063f2bf
Philipp Thürwächter <pithu@thuerwaechter.de>
2016-11-26 16:49:41 +0100

    Load moment tzdb zones and pack to build

--- src/MomentZoneRulesProvider.js
+++ src/MomentZoneRulesProvider.js
@@ -1,6 +1,14 @@
+import latest from 'moment-timezone/data/packed/latest';
+
 import { ZoneRulesProvider } from 'js-joda';
 
 import { MomentZoneRules } from './MomentZoneRules';
+
+let TZDB_VERSION = null;
+const AVAILABLE_ZONE_IDS = [];
+
+let zones = {};
+let links = {};
 
 export class MomentZoneRulesProvider extends ZoneRulesProvider {
     /**
@@ -26,8 +34,30 @@ export class MomentZoneRulesProvider extends ZoneRulesProvider {
      * @return {string[]} a modifiable copy of the set of zone IDs, not null
      */
     static getAvailableZoneIds(){
-        return [];
+        return AVAILABLE_ZONE_IDS;
     }
 
+    static unpackZoneInfo(packedZoneInfo) {
+        return packedZoneInfo.split('|');
+    }
 
+    static loadData(packedJson){
+        TZDB_VERSION = packedJson.version;
+
+        for (const packedZoneInfo of packedJson.zones) {
+            const tzdbZoneInfo = MomentZoneRulesProvider.unpackZoneInfo(packedZoneInfo);
+            AVAILABLE_ZONE_IDS.push(tzdbZoneInfo[0]);
+            zones[tzdbZoneInfo[0]] = tzdbZoneInfo;
+        }
+
+        for (const packedLink of packedJson.links) {
+            const link = packedLink.split('|');
+            AVAILABLE_ZONE_IDS.push(link[1]);
+            links[link[1]] = link[0];
+        }
+
+        console.log(TZDB_VERSION, AVAILABLE_ZONE_IDS);
+    }
 }
+
+MomentZoneRulesProvider.loadData(latest);
