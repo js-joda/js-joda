a964ee3608cadc8585fed235ba5f1fbedb09f942
Philipp Thürwächter <pithu@thuerwaechter.de>
2016-11-28 14:47:08 +0100

    Add copyright disclaimer

--- src/MomentZoneRulesProvider.js
+++ src/MomentZoneRulesProvider.js
@@ -1,3 +1,9 @@
+/*
+ * @copyright (c) 2016, Philipp Thürwächter, Pattrick Hüper
+ * @copyright (c) 2007-present, Stephen Colebourne & Michael Nascimento Santos
+ * @license BSD-3-Clause (see LICENSE in the root directory of this source tree)
+ */
+
 import latest from 'moment-timezone/data/packed/latest';
 
 import {
--- test/MomentZoneRulesProviderTest.js
+++ test/MomentZoneRulesProviderTest.js
@@ -1,3 +1,8 @@
+/*
+ * @copyright (c) 2016, Philipp Thürwächter, Pattrick Hüper
+ * @license BSD-3-Clause (see LICENSE in the root directory of this source tree)
+ */
+
 import { expect } from 'chai';
 
 import { ZoneRules } from 'js-joda';
