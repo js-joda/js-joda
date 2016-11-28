1733630a0ff097187b69f1888d11f753f1f5e3f2
Philipp Thürwächter <pithu@thuerwaechter.de>
2016-11-28 13:26:10 +0100

    Add test for available zone ids

--- test/MomentZoneRulesProviderTest.js
+++ test/MomentZoneRulesProviderTest.js
@@ -5,16 +5,14 @@ import { MomentZoneRules } from '../src/MomentZoneRules';
 import { MomentZoneRulesProvider } from '../src/MomentZoneRulesProvider';
 
 describe('MomentZoneRulesProvider', () => {
-
     context('getRules', () => {
-
         it('should return an instance of MomentZoneRules', () => {
             const zoneRules = MomentZoneRulesProvider.getRules('Europe/Berlin');
             expect(zoneRules).to.be.instanceOf(ZoneRules);
             expect(zoneRules).to.be.instanceOf(MomentZoneRules);
         });
 
-        it('should calculate isFixedOffset', () => {
+        it('should return fixed offset and ZoneRegions rules', () => {
             let zoneRules = MomentZoneRulesProvider.getRules('Europe/Berlin');
             expect(zoneRules.isFixedOffset()).to.be.false;
 
@@ -23,4 +21,19 @@ describe('MomentZoneRulesProvider', () => {
         });
     });
 
+    context('getAvailableZoneIds', () => {
+        it('should list some common zone id\'s', () => {
+            const availableZoneIds = MomentZoneRulesProvider.getAvailableZoneIds();
+
+            expect(availableZoneIds).contain('Australia/Darwin');
+            expect(availableZoneIds).contain('America/Argentina/Buenos_Aires');
+            expect(availableZoneIds).contain('Europe/Paris');
+            expect(availableZoneIds).contain('Asia/Kolkata');
+            expect(availableZoneIds).contain('Asia/Ho_Chi_Minh');
+
+            expect(availableZoneIds).contain('Etc/GMT+0');
+            expect(availableZoneIds).contain('Etc/GMT-1');
+            expect(availableZoneIds).contain('Etc/GMT+10');
+        });
+    });
 });
