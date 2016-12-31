42b36bef7640cd29a0018ed5947b55c2bdc13f25
pithu <pithu@thuerwaechter.de>
2016-12-31 18:23:33 +0100

    Add travis support with saucelabs and coveralls
    add browser and sauce labs test runner

--- src/license-preamble.js
+++ src/license-preamble.js
@@ -1,4 +1,2 @@
 //! @copyright (c) 2015-2016, Philipp Thürwächter, Pattrick Hüper & js-joda contributors
-//! @copyright (c) 2007-present, Stephen Colebourne & Michael Nascimento Santos
-//! @copyright (c) 2016, Tim Wood, github.com/moment/moment-timezone
 //! @license BSD-3-Clause (see LICENSE in the root directory of this source tree)
