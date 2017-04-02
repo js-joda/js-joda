593551804b23cd22101fb719b62834470e443903
pithu <pithu@thuerwaechter.de>
2017-04-02 18:17:40 +0200

    Organize imports

--- src/js-joda.js
+++ src/js-joda.js
@@ -3,10 +3,11 @@
  * @license BSD-3-Clause (see LICENSE in the root directory of this source tree)
  */
 
-export { Clock } from './Clock';
 export {
     DateTimeException, DateTimeParseException, IllegalArgumentException, IllegalStateException, NullPointerException
 } from './errors';
+
+export { Clock } from './Clock';
 export { DayOfWeek } from './DayOfWeek';
 export { Duration } from './Duration';
 export { Instant } from './Instant';
@@ -22,25 +23,25 @@ export { ZonedDateTime } from './ZonedDateTime';
 export { ZoneOffset } from './ZoneOffset';
 export { ZoneId } from './ZoneId';
 export { ZoneRegion } from './ZoneRegion';
+
 export { ZoneOffsetTransition } from './zone/ZoneOffsetTransition';
 export { ZoneRules } from './zone/ZoneRules';
 export { ZoneRulesProvider } from './zone/ZoneRulesProvider';
 
-export {convert} from './convert';
+export { ChronoField } from './temporal/ChronoField';
+export { ChronoUnit } from './temporal/ChronoUnit';
+export { IsoFields } from './temporal/IsoFields';
+export { TemporalAdjusters } from './temporal/TemporalAdjusters';
+export { TemporalQueries } from './temporal/TemporalQueries';
 
-export {nativeJs} from './temporal/NativeJsTemporal';
-export {ChronoField} from './temporal/ChronoField';
-export {ChronoUnit} from './temporal/ChronoUnit';
-export {IsoFields} from './temporal/IsoFields';
-export {TemporalAdjusters} from './temporal/TemporalAdjusters';
-export {TemporalQueries} from './temporal/TemporalQueries';
-
-export {DateTimeFormatter} from './format/DateTimeFormatter';
-export {DateTimeFormatterBuilder} from './format/DateTimeFormatterBuilder';
-export {ResolverStyle} from './format/ResolverStyle';
+export { DateTimeFormatter } from './format/DateTimeFormatter';
+export { DateTimeFormatterBuilder } from './format/DateTimeFormatterBuilder';
+export { ResolverStyle } from './format/ResolverStyle';
 
 import './_init';
 
-import { bindUse } from './use';
+export { convert } from './convert';
+export { nativeJs } from './temporal/NativeJsTemporal';
 
+import { bindUse } from './use';
 export const use = bindUse(exports);
