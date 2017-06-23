aa8cfaa010413ffa1dfad160caea612bf7e76777
Pattrick Hueper <phueper@hueper.net>
2017-06-23 09:22:14 +0200

    add some first very simple drafts of usage examples running in node and browser (using requirejs)

---
+++ examples/usage_node.js
@@ -0,0 +1,13 @@
+/*
+ * @copyright (c) 2017, Philipp Thuerwaechter & Pattrick Hueper
+ * @license BSD-3-Clause (see LICENSE.md in the root directory of this source tree)
+ */
+/* eslint-disable no-console, no-var */
+var joda = require('js-joda').use(require('js-joda-timezone')).use(require('../dist/js-joda-locale'));
+
+var zdt = joda.ZonedDateTime.of(2016, 1, 1, 0, 0, 0, 0, joda.ZoneId.of('Europe/Berlin'));
+// TODO: week fields don't work yet :/
+// console.log('formatted string:', zdt.format(joda.DateTimeFormatter.ofPattern('eeee yyyy MMMM dd, hh:mm:ss zzzz, \'Week \' W').withLocale(joda.Locale.ENGLISH)));
+console.log('formatted string:', zdt.format(joda.DateTimeFormatter.ofPattern('eeee yyyy MMMM dd, hh:mm:ss zzzz').withLocale(joda.Locale.ENGLISH)));
+console.log('formatted string:', zdt.format(joda.DateTimeFormatter.ofPattern('eeee yyyy MMMM dd, hh:mm:ss zzzz').withLocale(joda.Locale.GERMAN)));
+console.log('formatted string:', zdt.format(joda.DateTimeFormatter.ofPattern('eeee yyyy MMMM dd, hh:mm:ss zzzz').withLocale(joda.Locale.FRENCH)));
