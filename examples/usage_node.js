1caada0dfac65f8d28640cb24009b33f8fcf6cc3
Pattrick Hueper <phueper@hueper.net>
2017-07-31 13:52:05 +0200

    add TODO

--- examples/usage_node.js
+++ examples/usage_node.js
@@ -8,6 +8,7 @@ var joda = require('js-joda').use(require('js-joda-timezone')).use(require('../d
 var zdt = joda.ZonedDateTime.of(2016, 1, 1, 0, 0, 0, 0, joda.ZoneId.of('Europe/Berlin'));
 // TODO: week fields don't work yet :/
 // console.log('formatted string:', zdt.format(joda.DateTimeFormatter.ofPattern('eeee yyyy MMMM dd, hh:mm:ss zzzz, \'Week \' W').withLocale(joda.Locale.ENGLISH)));
+// TODO: add quarter, weeks and other fields?
 console.log('formatted string:', zdt.format(joda.DateTimeFormatter.ofPattern('eeee yyyy MMMM dd, hh:mm:ss zzzz').withLocale(joda.Locale.ENGLISH)));
 console.log('formatted string:', zdt.format(joda.DateTimeFormatter.ofPattern('eeee yyyy MMMM dd, hh:mm:ss zzzz').withLocale(joda.Locale.GERMAN)));
 console.log('formatted string:', zdt.format(joda.DateTimeFormatter.ofPattern('eeee yyyy MMMM dd, hh:mm:ss zzzz').withLocale(joda.Locale.FRENCH)));
