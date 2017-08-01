a52493e10558dcc5f6879884d7bea7e674b1c28d
Pattrick Hueper <phueper@hueper.net>
2017-08-01 23:47:47 +0200

    update examples to use more of the available fields

--- examples/usage_node.js
+++ examples/usage_node.js
@@ -6,9 +6,8 @@
 var joda = require('js-joda').use(require('js-joda-timezone')).use(require('../dist/js-joda-locale'));
 
 var zdt = joda.ZonedDateTime.of(2016, 1, 1, 0, 0, 0, 0, joda.ZoneId.of('Europe/Berlin'));
-// TODO: week fields don't work yet :/
-// console.log('formatted string:', zdt.format(joda.DateTimeFormatter.ofPattern('eeee yyyy MMMM dd, hh:mm:ss zzzz, \'Week \' W').withLocale(joda.Locale.ENGLISH)));
-// TODO: add quarter, weeks and other fields?
-console.log('formatted string:', zdt.format(joda.DateTimeFormatter.ofPattern('eeee yyyy MMMM dd, hh:mm:ss zzzz').withLocale(joda.Locale.ENGLISH)));
-console.log('formatted string:', zdt.format(joda.DateTimeFormatter.ofPattern('eeee yyyy MMMM dd, hh:mm:ss zzzz').withLocale(joda.Locale.GERMAN)));
-console.log('formatted string:', zdt.format(joda.DateTimeFormatter.ofPattern('eeee yyyy MMMM dd, hh:mm:ss zzzz').withLocale(joda.Locale.FRENCH)));
+// var zdt = joda.ZonedDateTime.of(2016, 1, 1, 0, 0, 0, 0, joda.ZoneId.of('America/Chicago'));
+console.log('en_US formatted string:', zdt.format(joda.DateTimeFormatter.ofPattern('eeee MMMM dd yyyy GGGG, hh:mm:ss a zzzz, \'Week \' ww, \'Quarter \' QQQ').withLocale(joda.Locale.US)));
+console.log('en_GB formatted string:', zdt.format(joda.DateTimeFormatter.ofPattern('eeee MMMM dd yyyy GGGG, hh:mm:ss a zzzz, \'Week \' ww, \'Quarter \' QQQ').withLocale(joda.Locale.UK)));
+console.log('de_DE formatted string:', zdt.format(joda.DateTimeFormatter.ofPattern('eeee MMMM dd yyyy GGGG, hh:mm:ss a zzzz, \'Week \' ww, \'Quarter \' QQQ').withLocale(joda.Locale.GERMANY)));
+console.log('fr_FR formatted string:', zdt.format(joda.DateTimeFormatter.ofPattern('eeee MMMM dd yyyy GGGG, hh:mm:ss a zzzz, \'Week \' ww, \'Quarter \' QQQ').withLocale(joda.Locale.FRANCE)));
