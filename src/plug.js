1526d652c2c891674998b7725cfeddaebf5ea0c5
Philipp Thuerwaechter <pithu@thuerwaechter.de>
2018-02-02 12:28:31 +0100

    Allow access to the internal tzdb data
    Fix issue #29

--- src/plug.js
+++ src/plug.js
@@ -17,7 +17,8 @@ export default function (jsJoda) {
     // TODO This is temporay solution to get access to the tzdb data.
     // It would be nice to decouple load and set tzdb data
     // and to provide a possiblitiy to load only a subset of zoneIds.
-    // Be aware that the format of the tzdb might change in the future!
+    // Be aware that this is an undocumented feature and
+    // that the format of the tzdb might change in the future!
     jsJoda.ZoneRulesProvider._TZDB = latest;
 
     extendSystemDefaultZoneId(jsJoda.ZoneId);
