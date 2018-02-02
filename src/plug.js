507e1f5cba48c8feb849dbfe3710509ec5dfc9f2
Philipp Thuerwaechter <pithu@thuerwaechter.de>
2018-02-02 12:26:50 +0100

    Allow access to the internal tzdb data

--- src/plug.js
+++ src/plug.js
@@ -13,6 +13,13 @@ MomentZoneRulesProvider.loadData(latest);
 export default function (jsJoda) {
     jsJoda.ZoneRulesProvider.getRules = MomentZoneRulesProvider.getRules;
     jsJoda.ZoneRulesProvider.getAvailableZoneIds = MomentZoneRulesProvider.getAvailableZoneIds;
+
+    // TODO This is temporay solution to get access to the tzdb data.
+    // It would be nice to decouple load and set tzdb data
+    // and to provide a possiblitiy to load only a subset of zoneIds.
+    // Be aware that the format of the tzdb might change in the future!
+    jsJoda.ZoneRulesProvider._TZDB = latest;
+
     extendSystemDefaultZoneId(jsJoda.ZoneId);
     return jsJoda;
 }
