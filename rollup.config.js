babed4900ae748f75b85490257d7d59f7252b173
Jakub <netghost03@gmail.com>
2018-03-02 12:15:05 +0100

    Add babe-preset-env and rollup-babel-plugin
    Replaced babel-preset-2015 with preset-env

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,10 @@
+import babel from 'rollup-plugin-babel';
+
 export default {
     input: './src/js-joda.js',
+    plugins: [
+        babel()
+    ],
     output: {
         file: 'dist/js-joda.esm.js',
         format: 'es'
