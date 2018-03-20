52f44a62912ffe7abf5ac5c1f3e106e1d4e112c1
Philipp Thuerwaechter <pithu@thuerwaechter.de>
2018-03-20 21:39:05 +0100

    Fix tests

--- test/useMomentZoneRules.js
+++ test/useMomentZoneRules.js
@@ -3,8 +3,11 @@
  * @license BSD-3-Clause (see LICENSE in the root directory of this source tree)
  */
 
+import latest from 'moment-timezone/data/packed/latest';
 import { ZoneRulesProvider, ZoneId } from 'js-joda';
 
 import plug from '../src/plug';
 
 plug({ ZoneRulesProvider, ZoneId });
+
+ZoneRulesProvider.loadTzdbData(latest);
