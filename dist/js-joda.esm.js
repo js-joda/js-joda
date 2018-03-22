61c873ea7bb990768cc6fcfc09073209a0709972
Philipp Thuerwaechter <pithu@thuerwaechter.de>
2018-03-22 19:12:36 +0100

    Bump version

--- dist/js-joda.esm.js
+++ dist/js-joda.esm.js
@@ -1,4 +1,4 @@
-//! @version js-joda - 1.8.1
+//! @version js-joda - 1.8.2
 //! @copyright (c) 2015-present, Philipp Thürwächter, Pattrick Hüper & js-joda contributors
 //! @copyright (c) 2007-present, Stephen Colebourne & Michael Nascimento Santos
 //! @license BSD-3-Clause (see LICENSE in the root directory of this source tree)
