59116852db8eb25693efda4fbb05efb46be4aec6
Philipp Thuerwaechter <pithu@thuerwaechter.de>
2018-03-22 18:46:35 +0100

    Add FIXME comment

--- test/run-node-samples.sh
+++ test/run-node-samples.sh
@@ -6,5 +6,6 @@ node ./examples/node/node-modules-03.js
 
 node ./examples/node/es6-index.js
 
+# FIXME
 ./node_modules/.bin/webpack --config ./examples/webpack/webpack.config.js
 node ./examples/webpack/webpack-bundle.js
