0e18a89a7031ae196246d643946a63a44dda9f68
Philipp Thuerwaechter <pithu@thuerwaechter.de>
2018-03-22 17:39:34 +0100

    Add es6 example

---
+++ examples/node/es6-index.js
@@ -0,0 +1,3 @@
+require('babel-register');
+
+require('./es6-modules-01');
--- test/run-node-samples.sh
+++ test/run-node-samples.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env bash
 
-node ./examples/node/*.js
+node ./examples/node/node-modules-01.js
+node ./examples/node/node-modules-02.js
+
+node ./examples/node/es6-index.js
