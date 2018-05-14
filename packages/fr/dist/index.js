8e384141cb191c8cbf35b2844cab15ace035f806
Pattrick Hueper <phueper@hueper.net>
2018-05-14 11:12:18 +0200

    bump version

--- packages/fr/dist/index.js
+++ packages/fr/dist/index.js
@@ -1,4 +1,4 @@
-//! @version js-joda-locale - 1.1.0-pre2+32.0.1
+//! @version js-joda-locale - 1.1.0-pre3+32.0.1
 //! @copyright (c) 2015-2016, Philipp Thürwächter, Pattrick Hüper & js-joda contributors
 //! @copyright (c) 2007-present, Stephen Colebourne & Michael Nascimento Santos
 //! @license BSD-3-Clause (see LICENSE in the root directory of this source tree)
