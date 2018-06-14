70bc4d1747f5b4133f1a8580b3f7eb5dd76d00ef
Philipp Thuerwaechter <pithu@thuerwaechter.de>
2018-06-14 22:58:07 +0200

    Fix example

--- examples/usage_node.js
+++ examples/usage_node.js
@@ -3,13 +3,14 @@
  * @license BSD-3-Clause (see LICENSE.md in the root directory of this source tree)
  */
 /* eslint-disable no-console, no-var */
-var joda = require('js-joda').use(require('../dist/js-joda-extra'));
+var joda = require('js-joda');
+var jodaExtra = require('../dist/js-joda-extra');
 
 var startInstant = joda.ZonedDateTime.of(2016, 11, 7, 0, 0, 0, 0, joda.ZoneOffset.UTC).toInstant();
 var innerInstant = startInstant.plus(1, joda.ChronoUnit.DAYS);
 var endInstant = startInstant.plus(2, joda.ChronoUnit.DAYS);
 var outerInstant = startInstant.plus(3, joda.ChronoUnit.DAYS);
-var interval = new joda.Interval(startInstant, endInstant);
+var interval = new jodaExtra.Interval(startInstant, endInstant);
 console.log('toString:', interval.toString());
 console.log('contains inner:', interval.contains(innerInstant));
 console.log('contains outer:', interval.contains(outerInstant));
