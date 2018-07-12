4eae0d64e361ca5fc515f64d6d31eca8db97e5f0
henryw374 <henryw374@gmail.com>
2018-07-12 07:44:52 +0100

    add OffsetClock and add withZone and equals to other Clock impls

--- test/ClockTest.js
+++ test/ClockTest.js
@@ -7,6 +7,9 @@ import {expect} from 'chai';
 import './_init';
 
 import {Clock} from '../src/Clock';
+import {Duration} from  '../src/Duration';
+import {Instant} from '../src/Instant';
+import {ZoneId} from '../src/ZoneId';
 
 describe('Clock', () => {
     describe('should deny calling an instant method of pseudo abstract class Clock', () => {
@@ -37,6 +40,41 @@ describe('Clock', () => {
         it('Fixed', () => {
             expect(Clock.fixed().toString()).to.contain('FixedClock');
         });
+        it('Offset', () => {
+            expect(Clock.offset().toString()).to.contain('OffsetClock');
+        });        
+    });
+    
+    describe('withZone', () => {
+        it('SystemDefaultClock', () => {
+            const base = Clock.systemDefaultZone();
+            expect(base.equals(base.withZone(ZoneId.systemDefault())));
+        });
+        it('FixedClock', () => {
+            const base = Clock.fixed(Instant.now(), ZoneId.systemDefault());
+            expect(base.equals(base.withZone(ZoneId.systemDefault())));
+        });                
+    });
+    
+    describe('offset clock', () => {
+    
+        const duration = Duration.parse('PT0H3M');
+        const base = Clock.fixed(Instant.now(), ZoneId.systemDefault());
+        const offset = Clock.offset(base, duration);
+                
+        it('is offset from base', () => {
+            expect(base.millis() === offset.millis() + duration.toMillis()); 
+        });
+        
+        it('equals', () => {
+            const offset2 = Clock.offset(base, duration);            
+            expect(offset.equals(offset2)); 
+        });
+        
+        it('with zone', () => {
+            const offset2 = offset.withZone(ZoneId.systemDefault());           
+            expect(offset.equals(offset2)); 
+        });            
     });
 });
 
