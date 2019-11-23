705d1f87893ff8098a1739189feb68cf99512d60
Pattrick Hueper <phueper@hueper.net>
2019-11-23 18:27:40 +0100

    update Test for german after update to cldr-data 36.0.0, texts have changed again

--- packages/locale/test/format/TextPrinterTest.js
+++ packages/locale/test/format/TextPrinterTest.js
@@ -109,8 +109,8 @@ describe('@js-joda/locale TextPrinterTest', () => {
                 [ChronoField.AMPM_OF_DAY, TextStyle.FULL, 0, 'AM'],
                 [ChronoField.AMPM_OF_DAY, TextStyle.FULL, 1, 'PM'],
 
-                [ChronoField.AMPM_OF_DAY, TextStyle.NARROW, 0, 'a'],
-                [ChronoField.AMPM_OF_DAY, TextStyle.NARROW, 1, 'p'],
+                [ChronoField.AMPM_OF_DAY, TextStyle.NARROW, 0, 'AM'],
+                [ChronoField.AMPM_OF_DAY, TextStyle.NARROW, 1, 'PM'],
 
                 [ChronoField.AMPM_OF_DAY, TextStyle.SHORT, 0, 'AM'],
                 [ChronoField.AMPM_OF_DAY, TextStyle.SHORT, 1, 'PM'],
