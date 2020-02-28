8efd16223661691081e4d9e36c54817b0259724b
Pattrick Hueper <phueper@hueper.net>
2020-02-28 20:51:50 +0100

    update locale build packages
    - add new locale packages

--- packages/locale/dist/prebuilt/it/index.js
+++ packages/locale/dist/prebuilt/it/index.js
@@ -1,4 +1,4 @@
-//! @version @js-joda/locale - 3.1.0+36.0.0
+//! @version @js-joda/locale - 3.1.1+36.0.0
 //! @copyright (c) 2015-2016, Philipp Thürwächter, Pattrick Hüper & js-joda contributors
 //! @copyright (c) 2007-present, Stephen Colebourne & Michael Nascimento Santos
 //! @license BSD-3-Clause (see LICENSE in the root directory of this source tree)
