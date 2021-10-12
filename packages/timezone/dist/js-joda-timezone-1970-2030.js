19eda501af517e23e00080c625fe1c0b3bc676b8
Philipp Thuerwaechter <pithu@thuerwaechter.de>
2021-10-12 20:46:11 +0200

    Publish
     - @js-joda/locale@4.3.0
     - @js-joda/timezone@2.8.0

--- packages/timezone/dist/js-joda-timezone-1970-2030.js
+++ packages/timezone/dist/js-joda-timezone-1970-2030.js
@@ -1,4 +1,4 @@
-//! @version @js-joda/timezone-2.7.1-2021c-1970-2030
+//! @version @js-joda/timezone-2.8.0-2021c-1970-2030
 //! @copyright (c) 2015-present, Philipp Thürwächter, Pattrick Hüper & js-joda contributors
 //! @license BSD-3-Clause (see LICENSE in the root directory of this source tree)
 (function webpackUniversalModuleDefinition(root, factory) {
