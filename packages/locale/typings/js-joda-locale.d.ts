d3701a6bc8a7448f0d52c3d119db7217eb0a82ee
Philipp Thuerwaechter <pithu@thuerwaechter.de>
2021-10-24 17:46:50 +0200

    Add Locale const values for all pre-builts

--- packages/locale/typings/js-joda-locale.d.ts
+++ packages/locale/typings/js-joda-locale.d.ts
@@ -22,6 +22,14 @@ export namespace Locale {
     const KOREAN: Locale;
     const JAPANESE: Locale;
     const JAPAN: Locale;
+    const ITALIAN: Locale;
+    const ITALY: Locale;
+    const CHINESE: Locale;
+    const ROMANIAN: Locale;
+    const SWEDISH: Locale;
+    const SWEDEN: Locale;
+    const HINDI: Locale;
+    const RUSSIAN: Locale;
 }
 
 import * as core from '@js-joda/core';
