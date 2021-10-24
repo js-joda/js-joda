3d99e517d69b5eb18af9415b62a6714f549a0dd9
Philipp Thuerwaechter <pithu@thuerwaechter.de>
2021-10-24 12:05:24 +0200

    Fix bash script

--- shared/git-push-docs-on-change.sh
+++ shared/git-push-docs-on-change.sh
@@ -1,13 +1,4 @@
 #!/bin/bash
 git add docs
-set +e  # Grep succeeds with nonzero exit codes to show results.
-git status | grep modified
-if [ $? -eq 0 ]
-then
-    set -e
-    git commit -m "docs updated on - $(date)" || true
-    git push || true
-else
-    set -e
-    echo "No changes since last run"
-fi
+git commit -m "docs updated on - $(date)" || true
+git push || true
