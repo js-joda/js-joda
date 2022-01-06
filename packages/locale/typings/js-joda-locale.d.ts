8a3487a06c07b0f7c2bd7d1f68fe13bff34cb414
gregm <greg.mccreath@origin.com.au>
2022-01-07 08:05:54 +1100

    WeekFields export and typings.  Remove toEpochSecond from OffsetTime typings.

--- packages/locale/typings/js-joda-locale.d.ts
+++ packages/locale/typings/js-joda-locale.d.ts
@@ -1,3 +1,5 @@
+import {DayOfWeek} from "@js-joda/core";
+
 export class Locale {
     public static getAvailableLocales(): string[];
 
@@ -10,31 +12,12 @@ export class Locale {
     public equals(other: any): boolean;
 }
 
-export class ComputedDayOfField {
-    public static ofDayOfWeekField(weekDef: WeekFields);
-    public static ofWeekOfMonthField(weekDef: WeekFields);
-    public static ofWeekOfYearField(weekDef: WeekFields);
-    public static ofWeekOfWeekBasedYearField(weekDef: WeekFields);
-    public static ofWeekBasedYearField(weekDef: WeekFields);
-    public getFrom(temporal: core.TemporalAccessor);
-    public adjustInto(temporal: core.Temporal, newValue: number);
-    public resolve(fieldValues: core.EnumMap, partialTemporal: core.TemporalAccessor, resolverStyle: core.ResolverStyle);
-    public name(): string;
-    public getBaseUnit(): core.TemporalUnit;
-    public getRangeUnit(): core.TemporalUnit;
-    public range(): core.ValueRange;
-    public isDateBased(): boolean;
-    public isTimeBased(): boolean;
-    public isSupportedBy(temporal: core.TemporalAccessor): boolean;
-    public rangeRefinedBy(temporal: core.TemporalAccessor): core.ValueRange;
-}
-
 export class WeekFields {
     public static ISO: WeekFields;
     public static SUNDAY_START: WeekFields;
     public static ofLocale(locale: Locale): WeekFields;
     public static of(firstDayOrLocale: core.DayOfWeek | Locale, minDays?: number);
-    public static ofFirstDayOfWeekMinDays(firstDayOfWeek, minimalDaysInFirstWeek);
+    public static ofFirstDayOfWeekMinDays(firstDayOfWeek: DayOfWeek, minimalDaysInFirstWeek: number);
     public firstDayOfWeek(): core.DayOfWeek;
     public minimalDaysInFirstWeek(): number;
     public dayOfWeek(): core.TemporalField;
@@ -86,21 +69,6 @@ declare module '@js-joda/core' {
         appendLocalizedOffset(textStyle: core.TextStyle): DateTimeFormatterBuilder;
     }
 
-    export interface Nameable {
-        name(): string;
-    }
-
-    export interface EnumMap {
-        putAll(otherMap: EnumMap): EnumMap;
-        containsKey(key: Nameable): boolean;
-        get(key: Nameable): any;
-        put(key: Nameable, val): EnumMap;
-        set(key: Nameable, val): EnumMap;
-        retainAll(keyList): EnumMap;
-        remove(key: Nameable): any;
-        keySet(): Record<string, any>;
-        clear();
-    }
 }
 
 export const __esModule: true;
