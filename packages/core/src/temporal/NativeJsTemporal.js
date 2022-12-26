5b310fb1c23b7ffae39f59b70ab83abf0b270d38
perceptron8 <perceptron8@users.noreply.github.com>
2022-12-26 14:20:28 +0100

    Update the copyright section

--- packages/core/src/temporal/NativeJsTemporal.js
+++ packages/core/src/temporal/NativeJsTemporal.js
@@ -1,5 +1,5 @@
 /*
- * @copyright (c) 2022, Philipp Thürwächter & Pattrick Hüper & Michał Sobkiewicz
+ * @copyright (c) 2015-present, Philipp Thürwächter, Pattrick Hüper & js-joda contributors
  * @license BSD-3-Clause (see LICENSE in the root directory of this source tree)
  */
 
