196c3fd82fab7acacdc88265192c9062381e03da
Philipp Thuerwaechter <pithu@thuerwaechter.de>
2024-04-19 20:17:56 +0200

    docs updated on - Fri Apr 19 20:17:56 CEST 2024

--- docs/inject/script/0-index.js
+++ docs/inject/script/0-index.js
@@ -1,4 +1,4 @@
-//! @version @js-joda/locale - 4.10.0
+//! @version @js-joda/locale - 4.11.0
 //! @copyright (c) 2015-present, Philipp Thürwächter, Pattrick Hüper & js-joda contributors
 //! @copyright (c) 2007-present, Stephen Colebourne & Michael Nascimento Santos
 //! @license BSD-3-Clause (see LICENSE in the root directory of this source tree)
